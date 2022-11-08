f640628c59ab0423939308b14542d94048c77a07
giovanni <giovanni.mazzocchin@gmail.com>
2022-11-08 18:24:47 +0100

    add test code for array algos

---
+++ cpp_lectures/test_basic_array_algorithms.cpp
@@ -0,0 +1,24 @@
+#include "basic_array_algorithms.h"
+#include <cstdlib>
+#include <cmath>
+#include <chrono>
+using namespace std;
+
+int main(){
+	srand(time(NULL));
+	const int size = 1000;
+	int array[size];
+	for (int i = 0; i < size; i++){
+		array[i] = rand() % 150;
+	}
+	
+	auto t0 = chrono::high_resolution_clock::now();
+	insertion_sort(array, size);
+	auto t1 = chrono::high_resolution_clock::now();
+	cout << "Insertion sort: " << chrono::duration_cast<chrono::nanoseconds>(t1 - t0).count() << " nanoseconds passed\n";
+	
+	auto t2 = chrono::high_resolution_clock::now();
+	bubble_sort(array, size);
+	auto t3 = chrono::high_resolution_clock::now();
+	cout << "Bubble sort: " << chrono::duration_cast<chrono::nanoseconds>(t3 - t2).count() << " nanoseconds passed\n";
+}
