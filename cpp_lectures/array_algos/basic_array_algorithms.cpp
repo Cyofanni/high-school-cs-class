e849ad31aa284fa4f9d26c7bcfc586a8dd0ec3b6
giovanni <giovanni.mazzocchin@gmail.com>
2022-11-18 19:48:40 +0100

    fixes

--- cpp_lectures/array_algos/basic_array_algorithms.cpp
+++ cpp_lectures/array_algos/basic_array_algorithms.cpp
@@ -43,17 +43,37 @@ void print_array_bubble_sort_debug(int* v, int size){
 	cout << endl;
 }
 
+
+void print_array_insertion_sort_debug(int* v, int size, int sorted_until){
+	int i = 0;
+	while (i < size){
+		if (i <= sorted_until){
+			cout << '*' << v[i] << '\t';
+		}
+		else {
+			cout << v[i] << '\t';
+		}
+		i++;
+	}
+	
+	cout << endl;
+}
+
+
 //insertion sort
-void insertion_sort(int* v, int size){
-	for (int i = 0; i < size; i++){
+void insertion_sort(int v[], int size){
+	print_array_insertion_sort_debug(v, size, 0);
+	
+	for (int i = 1; i < size; i++){
 		int item = v[i];
 		int j = i - 1;
 		while (j >= 0 && item < v[j]){
-			//cout << "inner iteration" << endl;
-			v[j] = v[j - 1];
+			v[j + 1] = v[j];
 			j--;
 		}
 		v[j + 1] = item;
+		
+		print_array_insertion_sort_debug(v, size, i);
 	}
 }
 
@@ -85,34 +105,64 @@ int binary_search_iterative(int* v, int size, int key){
 
 //naive bubble sort
 void bubble_sort_naive(int* v, int size){
-	bool swap_happened = false;
+	bool is_sorted = false;
 
-	do{
+	while (!is_sorted){
 		print_array_bubble_sort_debug(v, size);
-		swap_happened = false;  //suppose there won't be any swap, i.e. has been sorted
+		is_sorted = true;  //suppose there won't be any swap, i.e. has been sorted
 		for (int i = 0; i <= size - 2; i++){
 			if (v[i] > v[i + 1]){
 				int t = v[i];
 				v[i] = v[i + 1];
 				v[i + 1] = t;
-				swap_happened = true;
+				is_sorted = false;
 			}
 		}
 	}
-	while (swap_happened);
 }
 
 //bubble sort
+/*
+  [5, 8, 45, 35, 1]
+  Result of first scan, inner loop until index 3:
+	[5, 8, 35, 1, 45*] 
+  Result of second scan, inner loop until index 2:
+    [5, 8, 1, 35*, 45*]
+  Result of third scan, inner loop until index 1:
+    [5, 1, 8*, 35*, 45*]
+  Result of fourth scan, inner loop until index 0:
+    [1, 5*, 8*, 35*, 45*]
+*/
 void bubble_sort(int* v, int size){
-	for (int i = size - 1; i > 0; i--){
+	bool is_sorted = false;
+
+	for (int i = size - 1; i > 0 && is_sorted; i--){
+		is_sorted = true;
 		for (int j = 0; j < i; j++){
-			//cout << "inner iteration" << endl;
 			if (v[j] > v[j + 1]){
 				//swap
 				int t = v[j];
 				v[j] = v[j + 1];
 				v[j + 1] = t;
+				is_sorted = false;
 			}
 		}
+	}
+}
+
+//selection sort
+void selection_sort(int* v, int size){
+	for (int i = 0; i <= size - 1; i++){
+		int curr_smallest_item = v[i];
+		int curr_smallest_item_index = i;
+		for (int j = i; j <= size - 1; j++){
+			if (v[j] < curr_smallest_item){
+				curr_smallest_item = v[j];
+				curr_smallest_item_index = j;
+			}
+		}
+		int t = v[i];
+		v[i] = curr_smallest_item;
+		v[curr_smallest_item_index] = t;
 	}
 }
--- cpp_lectures/array_algos/test_basic_array_algorithms.cpp
+++ cpp_lectures/array_algos/test_basic_array_algorithms.cpp
@@ -6,19 +6,23 @@ using namespace std;
 
 int main(){
 	srand(time(NULL));
-	const int size = 1000;
+	const int size = 10;
 	int array[size];
 	for (int i = 0; i < size; i++){
 		array[i] = rand() % 150;
 	}
-	
-	auto t0 = chrono::high_resolution_clock::now();
+
+	//print_iterative(array, size);
 	insertion_sort(array, size);
-	auto t1 = chrono::high_resolution_clock::now();
-	cout << "Insertion sort: " << chrono::duration_cast<chrono::nanoseconds>(t1 - t0).count() << " nanoseconds passed\n";
+	//print_iterative(array, size);
 	
-	auto t2 = chrono::high_resolution_clock::now();
+	//auto t0 = chrono::high_resolution_clock::now();
+	//insertion_sort(array, size);
+	//auto t1 = chrono::high_resolution_clock::now();
+	//cout << "Insertion sort: " << chrono::duration_cast<chrono::nanoseconds>(t1 - t0).count() << " nanoseconds passed\n";
+	
+	/*auto t2 = chrono::high_resolution_clock::now();
 	bubble_sort(array, size);
 	auto t3 = chrono::high_resolution_clock::now();
-	cout << "Bubble sort: " << chrono::duration_cast<chrono::nanoseconds>(t3 - t2).count() << " nanoseconds passed\n";
+	cout << "Bubble sort: " << chrono::duration_cast<chrono::nanoseconds>(t3 - t2).count() << " nanoseconds passed\n";*/
 }
