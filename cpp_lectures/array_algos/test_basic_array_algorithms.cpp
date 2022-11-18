385a9409df338542e0bbd55c4530d7b7abaed8f1
giovanni <giovanni.mazzocchin@gmail.com>
2022-11-18 20:46:36 +0100

    fixes

--- cpp_lectures/array_algos/test_basic_array_algorithms.cpp
+++ cpp_lectures/array_algos/test_basic_array_algorithms.cpp
@@ -6,23 +6,26 @@ using namespace std;
 
 int main(){
 	srand(time(NULL));
-	const int size = 10;
+	const int size = 100000;
 	int array[size];
+	int array_copy[size];
+	
 	for (int i = 0; i < size; i++){
 		array[i] = rand() % 150;
+		array_copy[i] = array[i];
 	}
 
-	//print_iterative(array, size);
-	insertion_sort(array, size);
-	//print_iterative(array, size);
-	
-	//auto t0 = chrono::high_resolution_clock::now();
-	//insertion_sort(array, size);
-	//auto t1 = chrono::high_resolution_clock::now();
-	//cout << "Insertion sort: " << chrono::duration_cast<chrono::nanoseconds>(t1 - t0).count() << " nanoseconds passed\n";
-	
-	/*auto t2 = chrono::high_resolution_clock::now();
+	/*print_iterative(array, size);
 	bubble_sort(array, size);
+	print_iterative(array, size);*/
+	
+	auto t0 = chrono::high_resolution_clock::now();
+	insertion_sort(array, size);
+	auto t1 = chrono::high_resolution_clock::now();
+	cout << "Insertion sort:  " << chrono::duration_cast<chrono::nanoseconds>(t1 - t0).count() << " nanoseconds passed\n";
+	
+	auto t2 = chrono::high_resolution_clock::now();
+	bubble_sort(array_copy, size);
 	auto t3 = chrono::high_resolution_clock::now();
-	cout << "Bubble sort: " << chrono::duration_cast<chrono::nanoseconds>(t3 - t2).count() << " nanoseconds passed\n";*/
+	cout << "Bubble sort:    " << chrono::duration_cast<chrono::nanoseconds>(t3 - t2).count() << " nanoseconds passed\n";
 }
