af6f8be7522c1f02d11f1cf4c2fe22c0be28c55a
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2022-12-11 14:44:56 +0100

    removed tabs and trailing spaces

--- java_lectures/stack/StackChar.java
+++ java_lectures/stack/StackChar.java
@@ -1,54 +1,54 @@
 class StackChar{
-	private char[] stack_array;
-	private int top;
-	private int size;
-	
-	public StackChar(int size){
-		this.stack_array = new char[size];
-		this.top = -1;
-		this.size = size;
-	}
-	
-	public int push(char item){
-		if (top < size-1){
-			top++;
-			stack_array[top] = item;
-		}
-		
-		return top;
-	}
-	
-	public int pop(){ 
-		if (top >= 0){
-			top--;
-		}
-		
-		return top;
-	}
-	
-	public char peek() throws StackPeekException{
-		if (top >= 0){
-			return stack_array[top];
-		}
-		else {
-			throw new StackPeekException();
-		}
-	}
-	
-	public void printStack(){
-		for (int i = top; i >= 0; i--){
-			System.out.println("  [ " + stack_array[i] + " ]");
-			if (i > 0){
-				System.out.println("  ----------");
-			}
-		}
-	}
-	
-	public boolean is_empty(){
-		return top == -1;
-	}
-	
-	public boolean is_full(){
-		return top == size - 1;
-	}
+    private char[] stack_array;
+    private int top;
+    private int size;
+
+    public StackChar(int size){
+        this.stack_array = new char[size];
+        this.top = -1;
+        this.size = size;
+    }
+
+    public int push(char item){
+        if (top < size-1){
+            top++;
+            stack_array[top] = item;
+        }
+
+        return top;
+    }
+
+    public int pop(){
+        if (top >= 0){
+            top--;
+        }
+
+        return top;
+    }
+
+    public char peek() throws StackPeekException{
+        if (top >= 0){
+            return stack_array[top];
+        }
+        else {
+            throw new StackPeekException();
+        }
+    }
+
+    public void printStack(){
+        for (int i = top; i >= 0; i--){
+            System.out.println("  [ " + stack_array[i] + " ]");
+            if (i > 0){
+                System.out.println("  ----------");
+            }
+        }
+    }
+
+    public boolean is_empty(){
+        return top == -1;
+    }
+
+    public boolean is_full(){
+        return top == size - 1;
+    }
 }
--- java_lectures/stack/StackIntNode.java
+++ java_lectures/stack/StackIntNode.java
@@ -1,48 +1,48 @@
 class StackIntNode{
-	private int key;
-	private StackIntNode next;
-	private StackIntNode previous;
-	private boolean isBaseOfStack;
-	
-	StackIntNode(int key, StackIntNode next, StackIntNode previous, 
-				 boolean isBaseOfStack){
-		this.key = key;
-		this.next = next;
-		this.previous = previous;
-		this.isBaseOfStack = isBaseOfStack;
-	}
-	
-	StackIntNode push(int item){
-		/*"this" is the current stack pointer, must point to the new node
-		   returns a reference to the new top
-		   the new node cannot be the base: "isBaseOfStack" must be false
-		*/
-		this.next = new StackIntNode(item, null, this, false);
-		return this.next;
-	}
-	
-	StackIntNode pop(){
-		StackIntNode ref = this;
-		if (this.isBaseOfStack == false){
-			//previous doesn't point to popped node anymore
-			this.previous.next = null;
-			ref = this.previous;
-		}
-		
-		return ref;
-	}
-	
-	boolean isEmpty(){
-		return this.isBaseOfStack;
-	}
-	
-	void print(){
-		if (this.isBaseOfStack == true){
-			return;
-		}
-		System.out.println("  " + this.key);
-		System.out.println("  ==========");
-		this.previous.print();
-		return;
-	}
+    private int key;
+    private StackIntNode next;
+    private StackIntNode previous;
+    private boolean isBaseOfStack;
+
+    StackIntNode(int key, StackIntNode next, StackIntNode previous,
+                 boolean isBaseOfStack){
+        this.key = key;
+        this.next = next;
+        this.previous = previous;
+        this.isBaseOfStack = isBaseOfStack;
+    }
+
+    StackIntNode push(int item){
+        /*"this" is the current stack pointer, must point to the new node
+           returns a reference to the new top
+           the new node cannot be the base: "isBaseOfStack" must be false
+        */
+        this.next = new StackIntNode(item, null, this, false);
+        return this.next;
+    }
+
+    StackIntNode pop(){
+        StackIntNode ref = this;
+        if (this.isBaseOfStack == false){
+            //previous doesn't point to popped node anymore
+            this.previous.next = null;
+            ref = this.previous;
+        }
+
+        return ref;
+    }
+
+    boolean isEmpty(){
+        return this.isBaseOfStack;
+    }
+
+    void print(){
+        if (this.isBaseOfStack == true){
+            return;
+        }
+        System.out.println("  " + this.key);
+        System.out.println("  ==========");
+        this.previous.print();
+        return;
+    }
 }
