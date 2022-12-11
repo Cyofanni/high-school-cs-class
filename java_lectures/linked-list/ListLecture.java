614a519ceb92c6baa6600c17dd5f67d3a674ce2a
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2022-12-11 14:32:50 +0100

    removed tabs and trailing spaces

--- java_lectures/linked-list/ListLecture.java
+++ java_lectures/linked-list/ListLecture.java
@@ -1,7 +1,7 @@
 class ListLecture{
-	public static void main(String[] args){
-		ListNode ln = new ListNode(5, new ListNode(7, null));
-		ln.printList();
-		System.out.println("ln's size is: " + ln.getSizeRecursive());
-	}
+    public static void main(String[] args){
+        ListNode ln = new ListNode(5, new ListNode(7, null));
+        ln.printList();
+        System.out.println("ln's size is: " + ln.getSizeRecursive());
+    }
 }
