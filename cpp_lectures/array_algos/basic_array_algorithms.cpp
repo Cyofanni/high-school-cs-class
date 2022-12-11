780c6de270aeeef8d8e9241e76bdae0ba68b4584
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2022-12-11 14:21:11 +0100

    removed tabs and trailing spaces

--- cpp_lectures/array_algos/basic_array_algorithms.cpp
+++ cpp_lectures/array_algos/basic_array_algorithms.cpp
@@ -2,130 +2,130 @@
 
 //prints an array using the [] syntax
 void print_iterative(int* buf, int size){
-	if (buf){
-		for (int i = 0; i < size; i++){
-			cout << buf[i] << '\t';
-		}
-		cout << '\n';
-	}
+    if (buf){
+        for (int i = 0; i < size; i++){
+            cout << buf[i] << '\t';
+        }
+        cout << '\n';
+    }
 }
 
 
 //prints an array using pointer arithmetic
 void print_iterative_ptr_arithm(int* buf, int size){
-	if (buf){
-		for (int i = 0; i < size; i++){
-			cout << *(buf + i) << '\t';
-		}
-		cout << '\n';
-	}
+    if (buf){
+        for (int i = 0; i < size; i++){
+            cout << *(buf + i) << '\t';
+        }
+        cout << '\n';
+    }
 }
 
 
 void print_array_bubble_sort_debug(int* v, int size){
-	int i = 0;
-	while (i < size){
-		if (i < size - 1){
-			if (v[i] > v[i + 1]){
-				cout << '*' << v[i] << "------*";
-			}
-			else {
-				cout << v[i] << '\t';
-			}
-		}
-		else {
-				cout << v[i] << '\t';
-		}
-	
-		i++;
-	}
-	
-	cout << endl;
+    int i = 0;
+    while (i < size){
+        if (i < size - 1){
+            if (v[i] > v[i + 1]){
+                cout << '*' << v[i] << "------*";
+            }
+            else {
+                cout << v[i] << '\t';
+            }
+        }
+        else {
+                cout << v[i] << '\t';
+        }
+
+        i++;
+    }
+
+    cout << endl;
 }
 
 
 void print_array_insertion_sort_debug(int* v, int size, int sorted_until){
-	int i = 0;
-	while (i < size){
-		if (i <= sorted_until){
-			cout << '*' << v[i] << '\t';
-		}
-		else {
-			cout << v[i] << '\t';
-		}
-		i++;
-	}
-	
-	cout << endl;
+    int i = 0;
+    while (i < size){
+        if (i <= sorted_until){
+            cout << '*' << v[i] << '\t';
+        }
+        else {
+            cout << v[i] << '\t';
+        }
+        i++;
+    }
+
+    cout << endl;
 }
 
 
 //insertion sort
 void insertion_sort(int v[], int size){
-	//print_array_insertion_sort_debug(v, size, 0);
-	
-	for (int i = 1; i < size; i++){
-		int item = v[i];
-		int j = i - 1;
-		while (j >= 0 && item < v[j]){
-			v[j + 1] = v[j];
-			j--;
-		}
-		v[j + 1] = item;
-		
-		//print_array_insertion_sort_debug(v, size, i);
-	}
+    //print_array_insertion_sort_debug(v, size, 0);
+
+    for (int i = 1; i < size; i++){
+        int item = v[i];
+        int j = i - 1;
+        while (j >= 0 && item < v[j]){
+            v[j + 1] = v[j];
+            j--;
+        }
+        v[j + 1] = item;
+
+        //print_array_insertion_sort_debug(v, size, i);
+    }
 }
 
 
 //iterative binary ("logarithmic") search
 int binary_search_iterative(int* v, int size, int key){
-	int l = 0;
-	int h = size - 1;
-	int m;
-	bool found = false;
-	int index_of_key = -1;
-	
-	while (!found && h >= l){
-		m = (l + h) / 2;
-		if (key == v[m]){
-			index_of_key = m;
-			found = true;
-		}
-		else if (key > v[m]){
-			l = m + 1;
-		}
-		else{
-			h = m - 1;
-		}
-	}
-	
-	return index_of_key;
+    int l = 0;
+    int h = size - 1;
+    int m;
+    bool found = false;
+    int index_of_key = -1;
+
+    while (!found && h >= l){
+        m = (l + h) / 2;
+        if (key == v[m]){
+            index_of_key = m;
+            found = true;
+        }
+        else if (key > v[m]){
+            l = m + 1;
+        }
+        else{
+            h = m - 1;
+        }
+    }
+
+    return index_of_key;
 }
 
 //naive bubble sort
 void bubble_sort_naive(int* v, int size){
-	bool is_sorted = false;
+    bool is_sorted = false;
 
-	while (!is_sorted){
-		//print_array_bubble_sort_debug(v, size);
-		is_sorted = true;  //suppose there won't be any swap, i.e. has been sorted
-		for (int i = 0; i <= size - 2; i++){
-			if (v[i] > v[i + 1]){
-				int t = v[i];
-				v[i] = v[i + 1];
-				v[i + 1] = t;
-				is_sorted = false;
-			}
-		}
-	}
+    while (!is_sorted){
+        //print_array_bubble_sort_debug(v, size);
+        is_sorted = true;  //suppose there won't be any swap, i.e. has been sorted
+        for (int i = 0; i <= size - 2; i++){
+            if (v[i] > v[i + 1]){
+                int t = v[i];
+                v[i] = v[i + 1];
+                v[i + 1] = t;
+                is_sorted = false;
+            }
+        }
+    }
 }
 
 //bubble sort
 /*
   [5, 8, 45, 35, 1]
   Result of first scan, inner loop until index 3:
-	[5, 8, 35, 1, 45*] 
+    [5, 8, 35, 1, 45*]
   Result of second scan, inner loop until index 2:
     [5, 8, 1, 35*, 45*]
   Result of third scan, inner loop until index 1:
@@ -134,35 +134,35 @@ void bubble_sort_naive(int* v, int size){
     [1, 5*, 8*, 35*, 45*]
 */
 void bubble_sort(int* v, int size){
-	bool is_sorted = false;
+    bool is_sorted = false;
 
-	for (int i = size - 1; i > 0 && !is_sorted; i--){
-		is_sorted = true;
-		for (int j = 0; j < i; j++){
-			if (v[j] > v[j + 1]){
-				//swap
-				int t = v[j];
-				v[j] = v[j + 1];
-				v[j + 1] = t;
-				is_sorted = false;
-			}
-		}
-	}
+    for (int i = size - 1; i > 0 && !is_sorted; i--){
+        is_sorted = true;
+        for (int j = 0; j < i; j++){
+            if (v[j] > v[j + 1]){
+                //swap
+                int t = v[j];
+                v[j] = v[j + 1];
+                v[j + 1] = t;
+                is_sorted = false;
+            }
+        }
+    }
 }
 
 //selection sort
 void selection_sort(int* v, int size){
-	for (int i = 0; i <= size - 1; i++){
-		int curr_smallest_item = v[i];
-		int curr_smallest_item_index = i;
-		for (int j = i + 1; j <= size - 1; j++){
-			if (v[j] < curr_smallest_item){
-				curr_smallest_item = v[j];
-				curr_smallest_item_index = j;
-			}
-		}
-		int t = v[i];
-		v[i] = curr_smallest_item;
-		v[curr_smallest_item_index] = t;
-	}
+    for (int i = 0; i <= size - 1; i++){
+        int curr_smallest_item = v[i];
+        int curr_smallest_item_index = i;
+        for (int j = i + 1; j <= size - 1; j++){
+            if (v[j] < curr_smallest_item){
+                curr_smallest_item = v[j];
+                curr_smallest_item_index = j;
+            }
+        }
+        int t = v[i];
+        v[i] = curr_smallest_item;
+        v[curr_smallest_item_index] = t;
+    }
 }
--- cpp_lectures/array_algos/test_basic_array_algorithms.cpp
+++ cpp_lectures/array_algos/test_basic_array_algorithms.cpp
@@ -5,27 +5,27 @@
 using namespace std;
 
 int main(){
-	srand(time(NULL));
-	const int size = 20;
-	int array[size];
-	int array_copy[size];
-	
-	for (int i = 0; i < size; i++){
-		array[i] = rand() % 150;
-		array_copy[i] = array[i];
-	}
+    srand(time(NULL));
+    const int size = 20;
+    int array[size];
+    int array_copy[size];
 
-	print_iterative(array, size);
-	selection_sort(array, size);
-	print_iterative(array, size);
-	
-	auto t0 = chrono::high_resolution_clock::now();
-	insertion_sort(array, size);
-	auto t1 = chrono::high_resolution_clock::now();
-	cout << "Insertion sort:  " << chrono::duration_cast<chrono::nanoseconds>(t1 - t0).count() << " nanoseconds passed\n";
-	
-	auto t2 = chrono::high_resolution_clock::now();
-	bubble_sort(array_copy, size);
-	auto t3 = chrono::high_resolution_clock::now();
-	cout << "Bubble sort:    " << chrono::duration_cast<chrono::nanoseconds>(t3 - t2).count() << " nanoseconds passed\n";
+    for (int i = 0; i < size; i++){
+        array[i] = rand() % 150;
+        array_copy[i] = array[i];
+    }
+
+    print_iterative(array, size);
+    selection_sort(array, size);
+    print_iterative(array, size);
+
+    auto t0 = chrono::high_resolution_clock::now();
+    insertion_sort(array, size);
+    auto t1 = chrono::high_resolution_clock::now();
+    cout << "Insertion sort:  " << chrono::duration_cast<chrono::nanoseconds>(t1 - t0).count() << " nanoseconds passed\n";
+
+    auto t2 = chrono::high_resolution_clock::now();
+    bubble_sort(array_copy, size);
+    auto t3 = chrono::high_resolution_clock::now();
+    cout << "Bubble sort:    " << chrono::duration_cast<chrono::nanoseconds>(t3 - t2).count() << " nanoseconds passed\n";
 }
