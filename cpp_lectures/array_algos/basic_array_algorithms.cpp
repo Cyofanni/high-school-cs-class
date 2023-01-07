e31e4abecb83597a6aba0021c205bbfa3b71fba2
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-01-07 21:20:31 +0100

    fix

--- cpp_lectures/array_algos/basic_array_algorithms.cpp
+++ cpp_lectures/array_algos/basic_array_algorithms.cpp
@@ -1,5 +1,7 @@
 #include "basic_array_algorithms.h"
 
+using namespace std;
+
 //prints an array using the [] syntax
 void print_iterative(int* buf, int size){
     if (buf){
--- cpp_lectures/array_algos/basic_array_algorithms.h
+++ cpp_lectures/array_algos/basic_array_algorithms.h
@@ -2,8 +2,8 @@
 
 
 struct pos_value{
-	double value;
-	int position;
+    double value;
+    int position;
 };
 
 //prints an array using the [] syntax
@@ -24,7 +24,7 @@ void print_array_bubble_sort_debug(int*, int);
 /*left shifts a slice of an int array and fill the gap with a value passed
   as a parameter
     eg) left_shift_slice([5, 6*, 8, 2*, 9, 12], from: 1, to: 3, gap_fill: 999, size: 6)
-		---> [5, 8, 2, 999, 9, 12]
+        ---> [5, 8, 2, 999, 9, 12]
   Do nothing if slice limits ends go beyond array's size.
 */
 void left_shift_slice(int*, int, int, int, int);
@@ -33,7 +33,7 @@ void left_shift_slice(int*, int, int, int, int);
 /*right shifts a slice of an int array and fills in the gap with a value passed
   as a parameter
     eg) left_shift_slice([5, 6, 8, 2, 9, 12], from: 3, to: 5, gap_fill: 999, size: 6)
-		---> [5, 8, 2, 999, 2, 9]
+        ---> [5, 8, 2, 999, 2, 9]
     Do nothing if slice limits ends go beyond array's size.
 */
 void right_shift_slice(int*, int, int, int, int);
@@ -83,24 +83,3 @@ void bubble_sort_naive(int* v, int size);
 
 //bubble sort
 void bubble_sort(int*, int);
-
-
-//merges two sorted arrays
-void merge(int*, int, int*, int);
-
-
-//merge sort
-void merge_sort(int*, int);
-
-
-//prints upper triangular submatrix of a 5*5 matrix
-void print_triu(int[][5]);
-
-
-//prints upper triangular submatrix of a 5*5
-void print_triu(int[][5]);
-
-
-//verifies symmetry of a 5*5 matrix
-bool is_symmetric(int [][5]);
-
