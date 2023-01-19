3829a4e090b3d6da3b223343d914ccfe55c318f1
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-01-19 16:31:07 +0100

    add modular exponentiation

--- python/cryptography/modular_arithmetic.py
+++ python/cryptography/modular_arithmetic.py
@@ -6,13 +6,18 @@ def sum_mod(a, b, m):
 def product_mod(a, b, m):
     return (a * b) % m
 
+def exp_mod(base, exp, m):
+    return (base ** exp) % m
+
 def print_table(c, m):
     if (c == 's'):
         print("TABLE OF MODULAR SUM")
     elif (c == 'p'):
         print("TABLE OF MODULAR PRODUCT")
-
-    print()
+    elif (c == 'e'):
+        print("TABLE OF MODULAR EXPONENTIATION")
+    
+    print()    
     i = 0
     print('   ', end = '')
     while i < m:
@@ -31,14 +36,20 @@ def print_table(c, m):
                 res = sum_mod(i, j, m)
             elif c == 'p':
                 res = product_mod(i, j, m)
+            elif c == 'e':
+                res = exp_mod(i, j, m)
             print(res, ' ', end = '')
             j += 1
         print()
         i += 1
-
+    
 
 if len(sys.argv) < 3:
+    print("usage: 'python3 modular_arithmetic.py s 7' prints table of sum modulo 7")
+    print("usage: 'python3 modular_arithmetic.py p 7' prints table of product modulo 7")
+    print("usage: 'python3 modular_arithmetic.py e 7' prints table of exponentiation modulo 7")    
     exit("missing operation and modulo number")
+elif sys.argv[1] != 's' and sys.argv[1] != 'p' and sys.argv[1] != 'e':
+    exit("operation character must be 's' (sum), 'p' (product) or 'e' (exponentiation)")
 
 print_table(sys.argv[1], int(sys.argv[2]))
-
