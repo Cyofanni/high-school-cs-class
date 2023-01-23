fde08ed9e7b271ec2cb18a093662dd4243db1661
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-01-23 19:42:14 +0100

    improve buffer overflow with function pointers

--- c_lectures/hacking/buf_overflow.c
+++ c_lectures/hacking/buf_overflow.c
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 /* Buffer overflow with ip redirection.
    Simple example with constant data.
    echo 0 > /proc/sys/kernel/randomize_va_space
-   gcc -fno-stack-protector -g -o buf_overflow buf_overflow.c 
+   gcc -fno-stack-protector -g -o buf_overflow buf_overflow.c
 */
 
-int hackers_function() {
-  puts("greetings from the hacker");
+void hackers_function() {
+  puts("greetings from hackers function");
 }
 
-int f() {
+void f() {
   char str[8];
   puts("hello f");
-  str[16] = 0x69;
-  str[17] = 0x51;
+  int16_t ptr_as_int = (int16_t) &hackers_function;
+  int8_t byte_0 = ptr_as_int & 0x00FF;
+  int8_t byte_1 = (ptr_as_int & 0xFF00) >> 8;
+
+  str[20] = byte_0;
+  str[21] = byte_1;
 }
 
 int main() {
-  printf("hackers function at address: %p\n", hackers_function);
   f();
   puts("returned from call to f");
 }
