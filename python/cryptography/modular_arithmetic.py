1b0fdf9f9375e088be06d8f18dc6fe3698b3ec24
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-01-23 20:01:39 +0100

    add fermat's little theorem verifier function

--- python/cryptography/modular_arithmetic.py
+++ python/cryptography/modular_arithmetic.py
@@ -6,8 +6,13 @@ def sum_mod(a, b, m):
 def product_mod(a, b, m):
     return (a * b) % m
 
-def exp_mod(base, exp, m):
+def naive_exp_mod(base, exp, m):
     return (base ** exp) % m
+
+#Fermat's little theorem:
+# if m is prime and a is not a multiple of m => (a^(m-1)) mod m = 1
+def little_fermat_verify(base, m):
+    return naive_exp_mod(base, m - 1, m) == 1
 
 def print_table(c, m):
     if (c == 's'):
@@ -16,8 +21,8 @@ def print_table(c, m):
         print("TABLE OF MODULAR PRODUCT")
     elif (c == 'e'):
         print("TABLE OF MODULAR EXPONENTIATION")
-    
-    print()    
+
+    print()
     i = 0
     print('   ', end = '')
     while i < m:
@@ -37,19 +42,27 @@ def print_table(c, m):
             elif c == 'p':
                 res = product_mod(i, j, m)
             elif c == 'e':
-                res = exp_mod(i, j, m)
+                res = naive_exp_mod(i, j, m)
             print(res, ' ', end = '')
             j += 1
         print()
         i += 1
-    
 
 if len(sys.argv) < 3:
     print("usage: 'python3 modular_arithmetic.py s 7' prints table of sum modulo 7")
     print("usage: 'python3 modular_arithmetic.py p 7' prints table of product modulo 7")
-    print("usage: 'python3 modular_arithmetic.py e 7' prints table of exponentiation modulo 7")    
+    print("usage: 'python3 modular_arithmetic.py e 7' prints table of exponentiation modulo 7")
+    print("usage: 'python3 modular_arithmetic.py f 7' very Fermat's little theorem on m = 7")
     exit("missing operation and modulo number")
-elif sys.argv[1] != 's' and sys.argv[1] != 'p' and sys.argv[1] != 'e':
-    exit("operation character must be 's' (sum), 'p' (product) or 'e' (exponentiation)")
+elif sys.argv[1] != 's' and sys.argv[1] != 'p' and sys.argv[1] != 'e' and sys.argv[1] != 'f':
+    exit("operation character must be 's' (sum), 'p' (product), 'e' (exponentiation), " 
+            "'f' to verify Fermat's little theorem")
 
-print_table(sys.argv[1], int(sys.argv[2]))
+if sys.argv[1] == 'f':
+    f = little_fermat_verify(int(sys.argv[2]), int(sys.argv[3]))
+    if f == True:
+        print("your numbers agree with Fermat's theorem")
+    else:
+        print("your numbers do not agree with Fermat's theorem")
+else:
+    print_table(sys.argv[1], int(sys.argv[2]))
