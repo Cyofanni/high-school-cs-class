ffebb3a13be4fd2f8e42254fc23ec393fda368a4
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-02-02 18:41:18 +0100

    fix bugs

--- c_lectures/file_IO/hospital/hospital_management.c
+++ c_lectures/file_IO/hospital/hospital_management.c
@@ -8,6 +8,8 @@
 
 /* int fseek(FILE *stream, long offset, int whence); */
 
+/* char *fgets(char *s, int size, FILE *stream); */
+
 typedef struct patient {
   unsigned int id;
   char name[20];
@@ -17,26 +19,79 @@ typedef struct patient {
 } patient;
 
 int main() {
-  FILE *f_ptr = fopen("patients", "w+");
-  patient p1 = {5, "john", "smith", "washington", 4};
-  patient p2 = {9, "jack", "white", "washington", 6};
-  patient p3 = {10, "joseph", "wright", "san francisco", 4};
+  FILE *f_ptr = fopen("patients", "r");
+  int input_id;
+  char input_name[20] = {};
+  char input_surname[20] = {};
+  char input_residence_city[20] = {};
+  unsigned int input_num_exams;
 
-  fwrite(&p1, sizeof(patient), 1, f_ptr);
-  fwrite(&p2, sizeof(patient), 1, f_ptr);
-  fwrite(&p3, sizeof(patient), 1, f_ptr);
+  printf("enter a record (id, name, surname, city, number_of_exams): ");
+  putchar('\n');
+  printf(" id: ");
+  fscanf(stdin, "%d", &input_id);
 
-  unsigned int id;
+  printf(" name: ");
+  fscanf(stdin, "%s", input_name);
+
+  printf(" surname: ");
+  fscanf(stdin, "%s", input_surname);
+
+  printf(" residence city: ");
+  fscanf(stdin, "%s", input_residence_city);
+
+  printf(" number of exams: ");
+  fscanf(stdin, "%d", &input_num_exams);
+
+  patient p = {};
+
+  while (input_id != -1) {
+    p.id = input_id;
+    strncpy(p.name, input_name, 20);
+    strncpy(p.surname, input_surname, 20);
+    strncpy(p.residence_city, input_residence_city, 20);
+    p.num_exams = input_num_exams;
+
+    fwrite(&p, sizeof(patient), 1, f_ptr);
+
+    printf("enter a record (id, name, surname, city, number_of_exams): ");
+    putchar('\n');
+    printf(" id: ");
+    fscanf(stdin, "%d", &input_id);
+
+    printf(" name: ");
+    fscanf(stdin, "%s", input_name);
+
+    printf(" surname: ");
+    fscanf(stdin, "%s", input_surname);
+
+    printf(" residence city: ");
+    fscanf(stdin, "%s", input_residence_city);
+
+    printf(" number of exams: ");
+    fscanf(stdin, "%d", &input_num_exams);
+  }
+
+  //read the file
+  int i = 1;
+  int id;
+  char n[20] = {};
+  char s[20] = {};
+  char r[20] = {};
+  int n_e;
+
   rewind(f_ptr);
+  printf("%-4s%-20s%-20s%-20s%-4s\n", "id", "name", "surname", "residence city", "number of exams");
+  while (i <= 2) {
+    fread(&id, sizeof(int), 1, f_ptr);
+    fread(n, sizeof(char), 20, f_ptr);
+    fread(s, sizeof(char), 20, f_ptr);
+    fread(r, sizeof(char), 20, f_ptr);
+    fread(&n_e, sizeof(int), 1, f_ptr);
 
-  fread(&id, sizeof(int), 1, f_ptr);
-  printf("%d\n", id);
-  fseek(f_ptr, sizeof(patient), SEEK_SET);
+    printf("%-4d%-20s%-20s%-20s%-4d\n", id, n, s, r, n_e);
 
-  fread(&id, sizeof(int), 1, f_ptr);
-  fprintf(stdout, "%d\n", id);
-  fseek(f_ptr, sizeof(patient)*2, SEEK_SET);
-
-  fread(&id, sizeof(int), 1, f_ptr);
-  fprintf(stdout, "%d\n", id);
+    fseek(f_ptr, sizeof(patient) * i, SEEK_SET);
+    i++;
+  }
 }
