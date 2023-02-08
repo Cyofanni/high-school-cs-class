fb7e4ce6212be6032a91520e054c136c2f5e1ab0
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-02-08 17:30:47 +0100

    bug fix in factorization

--- python/cryptography/factorization.py
+++ python/cryptography/factorization.py
@@ -2,22 +2,26 @@
 import sys
 import math
 import time
+import sympy
 
 def factorize(n):
     factors = []
-    while n != 1:
-        i = 2
-        found_factor = False
-        sq_n = math.sqrt(n)
-        while i <= sq_n and found_factor == False:
-            if n % i == 0:
-                found_factor = True
-                factors.append(i)
-                n = n // i
-            i += 1
-        if i > sq_n:
-            factors.append(n)
-            n = n // n
+    if n == 0:
+        factors.append(n)
+    else:
+        while n != 1:
+            i = 2
+            found_factor = False
+            sq_n = int(math.sqrt(n))
+            while i <= sq_n and found_factor == False:
+                if n % i == 0:
+                    found_factor = True
+                    factors.append(i)
+                    n = n // i
+                i += 1
+            if found_factor == False:
+                factors.append(n)
+                n = n // n
 
     return factors
 
@@ -28,6 +32,9 @@ start_time = time.time()
 number = int(sys.argv[1])
 
 prime_factors = factorize(number)
-print('prime factors, with multiplicities are:', prime_factors)
-print("------ naive factorization took %s seconds to produce a result ------" %
-       (time.time() - start_time))
+prime_factors_sympy = sympy.factorint(number)
+print('prime factors, with multiplicities (sympy computation) are:', prime_factors_sympy)
+print('prime factors, with multiplicities (naive algorithm) are:', prime_factors)
+
+#print("------ naive factorization took %s seconds to produce a result ------" %
+       #(time.time() - start_time))
