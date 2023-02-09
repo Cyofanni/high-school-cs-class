81c5a5bc91e420dc6870aaa72cef360fc167053f
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-02-09 20:06:34 +0100

    add example with pointer

--- cpp_lectures/oop/inheritance/shapes.cpp
+++ cpp_lectures/oop/inheritance/shapes.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class shape {
 public:
-  virtual void draw() = 0;
+  virtual void draw() const = 0;
 };
 
 class square: public shape {
@@ -13,7 +13,7 @@ public:
   square(int size) {
     this->size = size;
   }
-  virtual void draw() {
+  virtual void draw() const {
     for (int i = 1; i <= size; i++) {
       if (i == 1 || i == size) {
 	for (int j = 1; j <= size; j++) {
@@ -39,7 +39,7 @@ public:
   line(int size) {
     this->size = size;
   }
-  virtual void draw() {
+  virtual void draw() const {
     for (int i = 1; i <= size; i++) {
       cout << '*';
     }
@@ -54,7 +54,7 @@ public:
   chessboard(int size) {
     this->size = size;
   }
-  virtual void draw() {
+  virtual void draw() const {
     for (int i = 1; i <= size; i++) {
       if (i % 2) {
 	cout << ' ';
@@ -68,8 +68,13 @@ public:
   }
 };
 
-void do_graphics(shape& s) {
+void do_graphics(const shape& s) {
   s.draw();
+  cout << endl << endl;
+}
+
+void do_graphics(const shape* s) {
+  s->draw();
   cout << endl << endl;
 }
 
@@ -81,4 +86,8 @@ int main() {
   do_graphics(sq);
   do_graphics(l);
   do_graphics(cb);
+  do_graphics(chessboard(7));
+  do_graphics(&sq);
+  do_graphics(&l);
+  do_graphics(&cb);
 }
