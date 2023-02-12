2e3cc328ba08013bf5884cde2812da19c4bdfa87
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-02-12 14:33:21 +0100

    add example on euler's theorem

---
+++ python/cryptography/euler.py
@@ -0,0 +1,14 @@
+import sys, sympy
+
+print('enter the integer a: ', end = '')
+a = int(input())
+print('enter the modulus: ', end = '')
+n = int(input())
+
+if sympy.gcd(a, n) == 1:
+    print('if a and n are coprime, euler\'s theorem states that (a^phi(n)) mod n is equal to: ', end = '')
+    print((a ** sympy.totient(n)) % n)
+    print('hence, the modular inverse of a is a^phi(n) - 1, which is equal to: ', end = '')
+    print((a ** (sympy.totient(n) - 1)) % n)
+else:
+    print('input numbers must be coprime')
