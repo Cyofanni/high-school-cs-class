c1aa16cdf2abe1e8ffa6925d75f6e9f0d4f69ccb
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-02-14 10:38:21 +0100

    fix to run the scripts on google colab

--- python/cryptography/euler.py
+++ python/cryptography/euler.py
@@ -1,9 +1,11 @@
+#usage python3 euler.py a n
 import sys, sympy
 
-print('enter an integer (a): ', end = '')
-a = int(input())
-print('enter the modulus (n): ', end = '')
-n = int(input())
+if len(sys.argv) < 3:
+    exit('too few command line arguments')
+
+a = int(sys.argv[1])
+n = int(sys.argv[2])
 
 if sympy.gcd(a, n) == 1:
     print('if a and n are coprime, euler\'s theorem states that (a^phi(n)) mod n is equal to: ', end = '')
