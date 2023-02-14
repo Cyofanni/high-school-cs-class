d4fd89ebeaddefeffeb0968e7f84b177e837ba0a
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-02-14 10:14:17 +0100

    fix parentheses

--- python/cryptography/euler.py
+++ python/cryptography/euler.py
@@ -8,7 +8,7 @@ n = int(input())
 if sympy.gcd(a, n) == 1:
     print('if a and n are coprime, euler\'s theorem states that (a^phi(n)) mod n is equal to: ', end = '')
     print((a ** sympy.totient(n)) % n)
-    print('hence, the modular inverse of a is a^phi(n) - 1, which is equal to: ', end = '')
+    print('hence, the modular inverse of a is a^(phi(n) - 1), which is equal to: ', end = '')
     print((a ** (sympy.totient(n) - 1)) % n)
 else:
     print('input numbers must be coprime')
