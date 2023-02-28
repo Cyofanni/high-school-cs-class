44c07e9e45711438a1f68c69aa35bf8c9079e3e8
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-02-28 19:41:05 +0100

    add flag for spaces

--- python/cryptography/rsa_apply_public_key.py
+++ python/cryptography/rsa_apply_public_key.py
@@ -6,5 +6,5 @@ public_key_exp = int(sys.argv[2])
 public_key_mod = int(sys.argv[3])
 
 ciphertext = pow(num, public_key_exp, public_key_mod)
-print('ciphertext is:', tnc.number_b_27_to_text(ciphertext))
-
+print('ciphertext is: ' + tnc.number_b_27_to_text(ciphertext), end = '')
+print('|')
