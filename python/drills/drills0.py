bcf9bb627840a2176dc4f63a33e0ceb970cd2e16
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-03-02 21:16:52 +0100

    runned pylint

--- python/drills/drills0.py
+++ python/drills/drills0.py
@@ -1,50 +1,51 @@
 import math
 
-def binary_search(ls, k):
-    ls.sort()
+def binary_search(lst, k):
+    lst.sort()
     low = 0
-    high = len(ls) - 1
+    high = len(lst) - 1
     found = False
 
-    while low < high and found == False:
+    while low < high and not found:
         middle = (low + high) // 2
-        if (ls[middle] == k):
+        if lst[middle] == k:
             found = True
-        elif k > ls[middle]:
+        elif k > lst[middle]:
             low = middle + 1
         else:
             high = middle - 1
 
     return (found, middle)
 
-def fibonacci(n):
-    l = []
-    l.append(0)
-    l.append(1)
-    for i in range(2, n):
-        l.append(l[i - 1] + l[i - 2])
+def fibonacci(num):
+    fib = []
+    fib.append(0)
+    fib.append(1)
+    for i in range(2, num):
+        fib.append(fib[i - 1] + fib[i - 2])
 
-    return l
+    return fib
 
-def merge(l1, l2):
+def merge(lst1, lst2):
     lmerged = []
-    l1.append(math.inf)
-    l2.append(math.inf)
-    size_lmerged = len(l1) + len(l2) - 2
+    lst1.append(math.inf)
+    lst2.append(math.inf)
+    size_lmerged = len(lst1) + len(lst2) - 2
     left_index = 0
     right_index = 0
 
     for i in range(size_lmerged):
-        if l1[left_index] <= l2[right_index]:
-            lmerged.append(l1[left_index])
+        if lst1[left_index] <= lst2[right_index]:
+            lmerged.append(lst1[left_index])
             left_index = left_index + 1
         else:
-            lmerged.append(l2[right_index])
+            lmerged.append(lst2[right_index])
             right_index = right_index + 1
 
     return lmerged
 
 #binary search
+print('binary search: ')
 l1 = [5, 1, 8, 3, 5, 3, 9, 12, 13]
 l2 = [8, 1, 8, 3, 5, 3, 9, 12, 13, 12, 24, 56, 45]
 l3 = ['mozart', 'beethoven', 'bach', 'the beatles',
@@ -56,9 +57,13 @@ t = binary_search(l3, 'beethoven')
 print(type(t))
 
 #fibonacci numbers
+print()
+print('fibonacci numbers: ')
 for i in range(10):
     print(fibonacci(i))
 
 #merge
+print()
+print('merge algorithm: ')
 print(merge([2, 4, 6, 10, 12], [6, 7, 10, 12, 13, 16, 17, 18, 20, 22]))
 print(merge([1, 18], [0, 2, 4]))
