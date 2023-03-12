a99e21d7108bed6171d985adc532b80391359678
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-03-12 10:41:33 +0100

    add examples on map and filter

---
+++ python/drills/functional_drills_1.py
@@ -0,0 +1,25 @@
+#map
+mozart_works = ('le nozze di figaro', 'così fan tutte', 'don giovanni'
+                'requiem', 'clarinet concerto K 622')
+
+mp = map(len, mozart_works)
+title_lengths = list(mp) #consumed
+mp1 = map(len, mozart_works)
+for i in mp1:
+    print(i)
+
+
+print()
+print()
+
+#filter
+def parity(num):
+    return num % 2 == 0
+
+numbers = [5, 6, 4, 1, 8, 76, 2, 4, 5]
+even_filter = filter(parity, numbers)
+for p in even_filter:
+    print(p)
+even_filter = filter(parity, numbers)
+evens = tuple(even_filter)
+print(evens)
