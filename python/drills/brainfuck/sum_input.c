3074441896eff978f43d5a21c581794b0ee0b683
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-03-19 20:54:26 +0100

    indent

--- python/drills/brainfuck/sum_input.c
+++ python/drills/brainfuck/sum_input.c
@@ -1,18 +1,21 @@
 #include <stdio.h>
-int main() {
-int data[30000] = {0};
-int data_pointer = 0;
-scanf("%d", &data[data_pointer]);
-data_pointer++;
-scanf("%d", &data[data_pointer]);
-data_pointer--;
-while (data[data_pointer]) {
-data[data_pointer]--;
-data_pointer++;
-data[data_pointer]++;
-data_pointer--;
-}
-data_pointer++;
-printf("%d", (data[data_pointer]));
-return 0;
+int
+main ()
+{
+  int data[30000] = { 0 };
+  int data_pointer = 0;
+  scanf ("%d", &data[data_pointer]);
+  data_pointer++;
+  scanf ("%d", &data[data_pointer]);
+  data_pointer--;
+  while (data[data_pointer])
+    {
+      data[data_pointer]--;
+      data_pointer++;
+      data[data_pointer]++;
+      data_pointer--;
+    }
+  data_pointer++;
+  printf ("%d", (data[data_pointer]));
+  return 0;
 }
