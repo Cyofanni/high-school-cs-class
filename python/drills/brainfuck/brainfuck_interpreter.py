4c725ede8bbb156b219dbd548cd984e351fbe905
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-03-19 14:06:49 +0100

    fix bug in interpreter

--- python/drills/brainfuck/brainfuck_interpreter.py
+++ python/drills/brainfuck/brainfuck_interpreter.py
@@ -9,10 +9,18 @@
 
 import sys
 
+tokens = ('>', '<', '+', '-', '.', ',', '[', ']')
+
+def keep_tokens(tk):
+    return tk in tokens
+
 program = ''
 with open(sys.argv[1], 'r') as file:
     for line in file:
         program += line
+
+filter_tokens = filter(keep_tokens, program)
+program = "".join(list(filter_tokens))
 
 program_size = len(program)
 data = []
@@ -21,30 +29,39 @@ for t in range(35000):
 
 instruction_pointer = 0
 data_pointer = 0
+
 while instruction_pointer < program_size:
     if program[instruction_pointer] == '>':
         data_pointer += 1
+        instruction_pointer += 1
     elif program[instruction_pointer] == '<':
         data_pointer -= 1
+        instruction_pointer += 1
     elif program[instruction_pointer] == '+':
         data[data_pointer] += 1
+        instruction_pointer += 1
     elif program[instruction_pointer] == '-':
         data[data_pointer] -= 1
+        instruction_pointer += 1
     elif program[instruction_pointer] == '.':
-        print(data[data_pointer], end = '')
+        print(data[data_pointer], end = '\n')
+        instruction_pointer += 1
     elif program[instruction_pointer] == ',':
         data[data_pointer] = input()
+        instruction_pointer += 1
     elif program[instruction_pointer] == '[':
         if data[data_pointer] == 0:
             iptr_after_jump = instruction_pointer
             while program[iptr_after_jump] != ']':
                 iptr_after_jump += 1
-            instruction_pointer = iptr_after_jump
+            instruction_pointer = iptr_after_jump + 1
+        else:
+            instruction_pointer += 1
     elif program[instruction_pointer] == ']':
         if data[data_pointer] != 0:
             iptr_after_jump = instruction_pointer
             while program[iptr_after_jump] != '[':
                 iptr_after_jump -= 1
-            instruction_pointer = iptr_after_jump
-
-    instruction_pointer += 1
+            instruction_pointer = iptr_after_jump + 1
+        else:
+            instruction_pointer += 1
---
+++ python/drills/brainfuck/program4.bf
@@ -0,0 +1,1 @@
+ move left+> < . > . <    [ -   > +  < ] >    print.
