bb65dd5025223c73e3a7dce9b4f7eb80f97da160
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-03-19 20:41:43 +0100

    add examples on variables and functions in php

---
+++ php/functions.php
@@ -0,0 +1,46 @@
+<?php
+//return reference
+function &f() {
+    $var = 7;
+    return $var;
+}
+
+$counter = 10;
+
+function inc_counter($k) {
+    //global counter not visible. $counter is local
+    $counter += $k;
+}
+
+function inc_counter_global($k) {
+    //global counter not visible. $counter is local
+    global $counter;
+    $counter += $k;
+}
+
+function inc_counter_global_r(&$k) {
+    //global counter not visible. $counter is local
+    global $counter;
+    $counter += $k;
+}
+
+function cnt() {
+    static $c = 0;
+    $c++;
+    echo "c value is: $c\n";
+}
+
+inc_counter(2);
+echo "$counter\n";
+inc_counter_global(2);
+echo "$counter\n";
+
+$k = 2;
+inc_counter_global_r($k);
+echo "$counter\n";
+
+cnt();
+cnt();
+cnt();
+cnt();
+?>
