7f118387aa08f3691f061520d7fa92712317ac9b
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-03-21 11:50:34 +0100

    remove scripts

--- python/mozart.py
+++
@@ -1,6 +0,0 @@
-year = 1756    #no need to write the type
-name = 'Wolfgang Amadeus Mozaer'   #no need to write the type
-work = 'The Magic Flute'
-
-print('{0} was born in {1}'.format(name, year))
-print('his most famous opera is {0}'.format(work))
