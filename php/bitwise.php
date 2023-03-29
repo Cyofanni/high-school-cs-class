004827c85b873d8e4bd14e04f4b7f74ff627c552
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-03-29 18:15:27 +0200

    add bitwise operators

--- php/bitwise.php
+++ php/bitwise.php
@@ -1,6 +1,28 @@
 <?php
-    $i = 1;
-    for ($j = 1; $j <= 8; $j++) {
-        echo $i << $j . "\n";
+function display_32_bits($n) {
+    $counter = 1;
+    $bits = array();
+    while ($counter <= 32) {
+        $bit = $n & 1 == 1 ? '1' : '0';
+        $bits[32 - $counter] = $bit;
+        $n = $n >> 1;
+        $counter++;
+        $var = 2;
     }
+    for ($i = 0; $i <= 31; $i++) {
+        echo $bits[$i];
+        if ($i < 31 && ($i + 1) % 4 == 0) {
+            echo '_';
+        }
+    }
+    echo "\n";
+}
+
+function twos_complement(&$n) {
+    return ~$n + 1;
+}
+
+for ($n = -16; $n <= 15; $n++) {
+    display_32_bits($n);
+}
 ?>
