edb8bbfedcc35c16aa5080ea03a32abb179e4196
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-03-30 20:24:51 +0200

    add assignment

---
+++ python/assignments/assignment_0.py
@@ -0,0 +1,20 @@
+#1. generate this list, using list comprehension:
+#   fracts = [1/2, 2/3, 3/4, 4/5 ..., 1023/1024];
+
+
+#2. generate this list, using list comprehension:
+#   fract_diffs = [fracts[1] - fracts[0], fracts[2] - fracts[1], fracts[3] - fracts[2] etc...]
+#   where fracts has been built at point 1;
+
+
+#3. generate a list containing Lucas numbers. Use list comprehension;
+
+#4. use filter to extract the odd numbers out of a list of integers;
+
+#5. use filter to extract powers of 2 out of a list of integers;
+
+#6. use filter to extract powers of 3 out of a list of integers;
+
+#7. use filter to extract the tuples whose sum is 16, out of a list of tuples;
+
+#8. use filter to extract powers of 2 out of a tuple of integers;
