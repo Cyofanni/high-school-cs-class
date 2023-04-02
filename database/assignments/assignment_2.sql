a9fc5eb6c6c039383b4de88d825bc8367a4ae103
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-04-02 15:23:00 +0200

    add difficult queries

--- database/assignments/assignment_2.sql
+++ database/assignments/assignment_2.sql
@@ -80,3 +80,31 @@ ON p1.parent_1_id = p2.id
 JOIN
 people AS p3
 ON p1.parent_2_id = p3.id;
+
+/*3. 
+write a query that produces this output:
++-------------+----------------+---------------+------------------+--------------------+---------------+------------------+--------------------+
+| person_name | person_surname | parent_1_name | parent_1_surname | parent_1_birthdate | parent_2_name | parent_2_surname | parent_2_birthdate |
++-------------+----------------+---------------+------------------+--------------------+---------------+------------------+--------------------+
+| maria       | bianchi        | silvia        | rossi            | 1925-04-12         | luigi         | bianchi          | 1930-04-12         |
++-------------+----------------+---------------+------------------+--------------------+---------------+------------------+--------------------+
+the output must include only people who have at least one parent born before January 1st, 1930.
+*/
+SELECT
+p1.name AS person_name,
+p1.surname AS person_surname,
+p2.name AS parent_1_name,
+p2.surname AS parent_1_surname,
+p2.birthdate AS parent_1_birthdate,
+p3.name AS parent_2_name,
+p3.surname AS parent_2_surname,
+p3.birthdate AS parent_2_birthdate
+FROM
+people AS p1
+JOIN
+people AS p2
+ON p1.parent_1_id = p2.id
+JOIN
+people AS p3
+ON p1.parent_2_id = p3.id
+WHERE p2.birthdate < '1930-01-01' OR p3.birthdate < '1930-01-01';
