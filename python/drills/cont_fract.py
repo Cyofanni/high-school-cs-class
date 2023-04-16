9fd8a9b73bcbf51ea4c866f8856de464a409bc93
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-04-16 10:50:42 +0200

    speed up convergence of continued fraction

--- python/drills/cont_fract.py
+++ python/drills/cont_fract.py
@@ -5,11 +5,20 @@ def sqrt_2_cont_fract(approx, num_calls):
         return approx
     return 1 + (1 / (1 + sqrt_2_cont_fract(approx, num_calls - 1)))
 
-for i in range(1, 33):
+approx1 = 1
+approx2 = 2
+for i in range(1, 7):
+    if i % 2 != 0:
+        approx2 = sqrt_2_cont_fract(approx1, i)
+        approx1 = sqrt_2_cont_fract(approx2, i)
+    else:
+        approx1 = sqrt_2_cont_fract(approx1, i)
+        approx2 = sqrt_2_cont_fract(approx2, i)
+
     print(str(i) + '-th approximation of sqrt(2)')
     print('from below', end = ' ')
-    print(sqrt_2_cont_fract(1, i))
+    print(approx1)
     print('from above', end = ' ')
-    print(sqrt_2_cont_fract(2, i), end = '\n\n')
+    print(approx2, end = '\n\n')
 
 print('math value: ', math.sqrt(2))
