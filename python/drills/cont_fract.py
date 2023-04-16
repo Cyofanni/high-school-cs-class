3f29e4fcb88d110da69caa894d103ab5ebd127c3
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-04-16 13:43:33 +0200

    add iterative continued fractions

--- python/drills/cont_fract.py
+++ python/drills/cont_fract.py
@@ -5,6 +5,11 @@ def sqrt_2_cont_fract(approx, num_calls):
         return approx
     return 1 + (1 / (1 + sqrt_2_cont_fract(approx, num_calls - 1)))
 
+def sqrt_2_cont_fract_iter(approx, num_iters):
+    for i in range(num_iters):
+        approx = 1 + (1 / (1 + approx))
+    return approx
+
 approx1 = 1
 approx2 = 2
 for i in range(1, 7):
@@ -15,10 +20,14 @@ for i in range(1, 7):
         approx1 = sqrt_2_cont_fract(approx1, i)
         approx2 = sqrt_2_cont_fract(approx2, i)
 
-    print(str(i) + '-th approximation of sqrt(2)')
+    print(str(i) + '-th approximation of sqrt(2), recursively')
     print('from below', end = ' ')
     print(approx1)
     print('from above', end = ' ')
     print(approx2, end = '\n\n')
 
+print('computed iteratively (16 iterations), starting from approx = 1: ', sqrt_2_cont_fract_iter(1, 33))
+print('computed iteratively (16 iterations), starting from approx = 2: ', sqrt_2_cont_fract_iter(2, 33))
+
+print()
 print('math value: ', math.sqrt(2))
