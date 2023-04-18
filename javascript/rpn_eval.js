3d211a3cbeac390d1d93a40a4809de3af84edd21
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-04-18 18:53:42 +0200

    fix rpn evaluation

--- javascript/rpn_eval.js
+++ javascript/rpn_eval.js
@@ -1,28 +1,29 @@
 /* expression in infix notation
-     (2 + 2) * 3 * 4 + 2
+     2 + 5 * 3
 */
-expr = '2 2 + 3 4 * 2 +';
+expr = '2 5 3 * +';
 stack = [];
 
 for (i = 0; i < expr.length; i++) {
     if (expr[i] != ' ' && isFinite(expr[i])) {
         stack.push(Number(expr[i]));
     }
-    else if (expr[i] == '+' || expr[i] == '*') {
+    else if (expr[i] == '+' || expr[i] == '*'){
+        console.log(stack);
+        var ans;
         if (expr[i] == '+') {
 	    ans = 0;
 	}
 	else if (expr[i] == '*') {
 	    ans = 1;
 	}
-        for (j = stack.length - 1; j >= 0; j--) {
+        for (j = 1 ; j <= 2; j++) {
 	    if (expr[i] == '+') {
-		ans += Number(stack[j]);
+		ans += Number(stack.pop());
 	    }
 	    else if (expr[i] == '*') {
-		ans *= Number(stack[j]);
+		ans *= Number(stack.pop());
 	    }
-	    stack.pop();
 	}
 	stack.push(ans);
     }
