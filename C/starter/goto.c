d9b9c9930e0cc2abaa4f2e868beea7c699012d41
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-04-26 15:09:01 +0200

    considered harmful

---
+++ C/starter/goto.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    /*ex 0
+    goto l1;
+    while (1) {};
+    l1:
+        puts("code of p1 label.");
+    */
+
+    /*ex 1
+    p1:
+        goto p2;
+    p2:
+        goto p1;
+    */
+
+    /*ex 2
+    unsigned int i = 1;
+    begin_loop:
+        if (i > 10)
+            goto end_loop;
+        printf("i: %u\n", i);
+        i++;
+        goto begin_loop;
+    end_loop:
+            printf("out of loop, i: %u\n", i);
+    */
+}
