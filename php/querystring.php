03ba0904c9604d5ece90e5fdff8e0f7be3f6ccd8
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-05-14 11:28:58 +0200

    add query string example

--- php/querystring.php
+++ php/querystring.php
@@ -1,5 +1,18 @@
 <?php
-  echo "<html><body>Hello ";
-  echo $_GET["name"];
+  /*
+     apache web server error log /var/log/apache2/
+  */
+
+  $db = new mysqli("localhost", "root", "brocchi", "hospital");
+  $name = $_GET['name'];
+  $surname = $_GET['surname'];
+  $q = "SELECT * FROM doctors WHERE name = '" . "$name" . "'" . " AND surname = '" . "$surname'";
+  $res = $db -> query($q);
+
+  echo "<html><body>";
+  while ($rec = $res -> fetch_assoc()) {
+      echo $rec['id'] . "  " . "  " . $rec['name'] . "  " . $rec['surname'];
+      echo "</br>";
+  }
   echo "</body></html>";
 ?>
