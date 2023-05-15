2ae445a3d261b2dc9cdb1492798e1871ac85715d
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-05-15 19:44:07 +0200

    compute shortest paths

--- python/optimization/dijkstra.py
+++ python/optimization/dijkstra.py
@@ -1,8 +1,10 @@
 #Dijkstra’s algorithm
 
 import math
-graph = {'s':{'t':10,'y':5},'y':{'t':3,'x':9,'z':2},'z':{'s':7,'x':6},'x':{'z':4},'t':{'x':1,'y':2}}
+graph = {'s':{'t':10,'y':5},'y':{'t':3,'x':9,'z':2},'z':{'s':7,'x':6},'x':{'z':4},
+         't':{'x':1,'y':2}}
 distances_source = {'s':0,'y':math.inf,'z':math.inf,'x':math.inf,'t':math.inf}
+predecessors_shortest_path = {'s':None,'y':None,'z':None,'x':None,'t':None}
 Q = distances_source.copy()
 
 def extract_min_cost_node():
@@ -10,7 +12,7 @@ def extract_min_cost_node():
   min_pair = None
   for pair in Q.items():
     if pair[1] < min_cost:
-      min_cost = pair[1]
+      min_cost = pair[1] 
       min_pair = pair
   Q.pop(min_pair[0])
   return min_pair[0]
@@ -25,7 +27,20 @@ while len(Q) > 0:
     distance_source_s_current_node = distances_source[current_node]
     if distance_source_s_current_node + succ_distance < distances_source[succ_key]:
       distances_source[succ_key] = distance_source_s_current_node + succ_distance
+      predecessors_shortest_path[succ_key] = current_node
       Q[succ_key] = distance_source_s_current_node + succ_distance
 
   print('costs from source, computed so far:', distances_source)
   print()
+
+node = 'x'
+print('shortest path to node:', node)
+shortest_path = [node]
+while predecessors_shortest_path[node] != None:
+  node = predecessors_shortest_path[node]
+  shortest_path.insert(0, node)
+
+print('BEGIN OF PATH', end = ' -> ')
+for node in shortest_path:
+  print(node, '->', end = ' ')
+print('END OF PATH')
