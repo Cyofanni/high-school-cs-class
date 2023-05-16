2f1ad6d217cf639a54183a06df627f6a5b512b25
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-05-16 12:33:21 +0200

    extract machine representation of doubles with unions

--- C/bits_n_bytes/floating_point.c
+++ C/bits_n_bytes/floating_point.c
@@ -1,25 +1,35 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <string.h>
 
-#define IEEE_754_SINGLE_SIZE 32
+#define IEEE_754_DOUBLE_SIZE 64
 
 union f_i {
-  float f;
-  unsigned int i;
+  double f;
+  uint64_t i;
 };
 
 int main() {
     union f_i f_i_var;
-    f_i_var.f = 0.5;
-    int8_t ieee754_bits[IEEE_754_SINGLE_SIZE];
+    f_i_var.f = 27.56640625;
+    printf("The machine representation (IEEE 754 - double precision) of %10.8lf is:\n",
+            f_i_var.f);
 
-    for (int j = 1; j <= IEEE_754_SINGLE_SIZE; j++) {
-      int8_t bit = f_i_var.i & 0x00000001;
-      ieee754_bits[IEEE_754_SINGLE_SIZE - j] = bit;
-      f_i_var.i = f_i_var.i >> 1;
+    char bits[IEEE_754_DOUBLE_SIZE + 1] = {};
+    for (int i = 1; i <= IEEE_754_DOUBLE_SIZE; i++) {
+        int8_t bit = f_i_var.i & 0x0000000000000001;
+        bits[IEEE_754_DOUBLE_SIZE - i] = bit == 1 ? '1' : '0';
+        f_i_var.i >>= 1;
     }
-    for (int j = 0; j < IEEE_754_SINGLE_SIZE; j++) {
-      printf("%d", ieee754_bits[j]);
+
+    for (int i = 0; i < strlen(bits); i++) {
+        fputc(bits[i], stdout);
+        if (i == 0) {
+            fputc(' ', stdout);
+        }
+        else if (i == 11) {
+            fputc(' ', stdout);
+        }
     }
     putchar('\n');
 }
