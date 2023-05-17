1ce0a86668b57f23b3670a96e6c0ebd51887ab46
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-05-17 09:50:55 +0200

    add easily sql-injectable login

---
+++ php/hospital/login.php
@@ -0,0 +1,20 @@
+<?php
+  $db = new mysqli("localhost", "root", "brocchi", "hospital");
+
+  $q = "SELECT * FROM doctors WHERE id = " . $_GET['id'] .
+       " AND password = " . $_GET['password'];
+
+  $res = $db -> query($q);
+  echo "<html><body>";
+  if (mysqli_num_rows($res) == 0) {
+    echo "login failed";
+    die("exiting login");
+  }
+  $res = $db -> query($q);
+  while ($rec = $res -> fetch_assoc()) {
+    echo $rec['name'] . "  " . $rec['surname'] . "  " . $rec['birthdate'] . "  "
+         . $rec['password'] . "\n";
+    echo "</br>";
+  }
+  echo "</body></html>";
+?>
