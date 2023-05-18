6aee3eda2e68254e3bdee9e71f3d8383b0620a2c
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-05-18 19:43:44 +0200

    fix buffer overflow example

--- C/hacking/buf_overflow.c
+++ C/hacking/buf_overflow.c
@@ -2,28 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 
-/* Buffer overflow with ip redirection.
-   Simple example with constant data.
+/* Buffer overflow with ip redirection;
+   Simple example with constant data;
    echo 0 > /proc/sys/kernel/randomize_va_space
    gcc -fno-stack-protector -g -o buf_overflow buf_overflow.c
 */
 
 void hackers_function() {
-  puts("greetings from hackers function");
+  puts("***HACKED***");
 }
 
 void f() {
-  char str[8];
-  puts("hello f");
-  int16_t ptr_as_int = (int16_t) &hackers_function;
-  int8_t byte_0 = ptr_as_int & 0x00FF;
-  int8_t byte_1 = (ptr_as_int & 0xFF00) >> 8;
-
-  str[20] = byte_0;
-  str[21] = byte_1;
+  int8_t buffer[8];
+  buffer[0] = 255;
+  puts("f (licit function) called");
+  buffer[16] = (int8_t)&hackers_function;
+  //buffer[16] = 0x69;
 }
 
 int main() {
+  printf("hacker's function's code starts at: %p\n", &hackers_function);
   f();
   puts("returned from call to f");
 }
