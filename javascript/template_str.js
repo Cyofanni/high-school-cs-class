1dc45574a2bfdd08b34bd3183e7cb3b1aba11507
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-05-21 17:16:58 +0200

    add example on template js strings

---
+++ javascript/template_str.js
@@ -0,0 +1,14 @@
+const firstName = 'giovanni';
+const orderedProduct = 'violin';
+const companyName = 'Musical Instruments Inc.';
+
+const email = `
+  Hello ${firstName},
+  Order Details:
+    ${orderedProduct}
+
+  Thank you,
+  ${companyName}
+`;
+
+console.log(email);
