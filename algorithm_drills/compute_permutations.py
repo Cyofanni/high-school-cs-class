33863a87825c1e7a650ace61a0d6564e9b215051
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-05-23 18:20:05 +0200

    shorten code

--- algorithm_drills/compute_permutations.py
+++ algorithm_drills/compute_permutations.py
@@ -8,9 +8,8 @@ def compute_permutations(word, k, permutation):
     return
 
   permutation[k] = word[k]
-  compute_permutations(word, k + 1, permutation)
 
-  for i in range(1, len(word)):
+  for i in range(0, len(word)):
     t = word[0]
     word[0] = word[i]
     word[i] = t
@@ -19,7 +18,7 @@ def compute_permutations(word, k, permutation):
     permutation[i] = t
     compute_permutations(word, k + 1, permutation)
 
-st = 'star'
+st = 'mozart'
 st_l = list(st)
 compute_permutations(st_l, 0, ['' for i in range(len(st_l))])
 print('#anagrams:', len(permutations))
