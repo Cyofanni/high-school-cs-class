ce469d07d1a771c4ee7196d96aecc7716be41172
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-05-23 18:05:26 +0200

    generate permutations inefficiently

---
+++ algorithm_drills/compute_permutations.py
@@ -0,0 +1,27 @@
+permutations = []
+
+def compute_permutations(word, k, permutation):
+  if k == len(word):
+    permutation_str = ''.join(permutation)
+    if permutation_str not in permutations:
+    permutations.append(permutation_str)
+    return
+
+  permutation[k] = word[k]
+  compute_permutations(word, k + 1, permutation)
+
+  for i in range(1, len(word)):
+    t = word[0]
+    word[0] = word[i]
+    word[i] = t
+    t = permutation[0]
+    permutation[0] = permutation[i]
+    permutation[i] = t
+    compute_permutations(word, k + 1, permutation)
+
+st = 'star'
+st_l = list(st)
+compute_permutations(st_l, 0, ['' for i in range(len(st_l))])
+print('#anagrams:', len(permutations))
+for p in permutations:
+  print(p)
