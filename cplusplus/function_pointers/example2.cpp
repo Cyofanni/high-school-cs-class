f6c3610bd6caefa81cc50872589bc632bff62de6
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-05-25 10:00:01 +0200

    fix code

--- cplusplus/function_pointers/example2.cpp
+++ cplusplus/function_pointers/example2.cpp
@@ -12,7 +12,7 @@ bool gte(int a, int b) {
 }
 
 int extract_min_or_max(int arr[], int size, bool (*cmp) (int, int)) {
-    int item = arr[1];
+    int item = arr[0];
     for (int i = 1; i < size; i++) {
         if ((*cmp)(arr[i], item)) {
             item = arr[i];
@@ -22,7 +22,4 @@ int extract_min_or_max(int arr[], int size, bool (*cmp) (int, int)) {
 }
 
 int main() {
-    int array[12] = {1, 5, 106, 3, -1, 2, 106, 76, -65, 3, 5, 2};
-    cout << extract_min_or_max(array, 12, &gte) << endl;
-    cout << extract_min_or_max(array, 12, &lte) << endl;
 }
