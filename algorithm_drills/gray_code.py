30ecd36b4eb25b674cb20f527ef7fde6a9d35cbd
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-05-29 22:29:59 +0200

    build gray code recursively

---
+++ algorithm_drills/gray_code.py
@@ -0,0 +1,22 @@
+codes = []
+
+def generate_gray_codes(k):
+  if k == 1:
+    codes.append([0])
+    codes.append([1])
+    return
+
+  generate_gray_codes(k - 1)
+  old_codes = codes.copy()
+  size_codes = len(codes)
+  for i in range(size_codes):
+    codes[i] = [0] + codes[i]
+  for i in range(size_codes):
+    codes.append([1] + old_codes[i])
+  size_codes = len(codes)
+  middle = size_codes // 2
+  codes[middle : size_codes] = codes[size_codes - 1 : middle - 1 : -1]
+
+generate_gray_codes(8)
+for code in codes:
+  print(code)
