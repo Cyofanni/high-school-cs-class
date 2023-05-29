daccf540536d6a374f6f0e0cba03724bc25dfc4d
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-05-29 21:54:00 +0200

    clean example on polymorphism

--- cplusplus/oop/inheritance/shapes.cpp
+++ cplusplus/oop/inheritance/shapes.cpp
@@ -4,10 +4,7 @@ using namespace std;
 //abstract class
 class shape {
 public:
-  int x;
   virtual void draw() const = 0;
-  virtual void move() = 0;
-  virtual int get_x() const = 0;
 };
 
 class square: public shape {
@@ -35,12 +32,6 @@ public:
     }
     cout << endl << endl;
   }
-  void move() {
-
-  }
-  int get_x() const {
-    return x;
-  }
 };
 
 class line: public shape {
@@ -55,12 +46,6 @@ public:
       cout << '*';
     }
     cout << endl << endl;
-  }
-  void move() {
-
-  }
-  int get_x() const {
-    return x;
   }
 };
 
@@ -83,57 +68,24 @@ public:
     }
     cout << endl << endl;
   }
-  void move() {
-
-  }
-  int get_x() const {
-    return x;
-  }
 };
 
-void do_graphics(const shape& s) {
-  s.draw();
-  cout << endl << endl;
-}
-
 void do_graphics(const shape* s) {
-  s->draw();
+  s -> draw();
   cout << endl << endl;
 }
 
 int main() {
   square sq(6);
-  sq.x = 10;
+  sq.draw();
   line l(13);
-  l.x = 20;
+  l.draw();
+  chessboard cb(10);
 
-  /*
   shape* s = &sq;
-  s->draw();
+  s -> draw();
   s = &l;
-  s->draw();
-  */
-
-  //example on integer reference
-  int a = 10, b = 20;
-  cout << "a: " << a << "  b: " << b << endl;
-  int& ref = a;
-  cout << "ref: " << ref << "  a: " << a << endl;
-  cout << "address of ref: " << &ref << "  address of a: " << &a << endl;
-  ref = b;
-  cout << "ref: " << ref << "  b: " << b << endl;
-  cout << "address of ref: " << &ref << "  address of b: " << &b << endl << endl;
-
-  //note that address of reference does not change
-  shape& sref = sq;
-  cout << "sref.x: " << sref.x << "   sq.x: " << sq.x << endl;
-  cout << "address of sref: " << &sref << "  address of sq: " << &sq << endl;
-  cout << "sref.get_x(): " << sref.get_x() << "   sq.x: " << sq.x << endl;
-  sref.draw();
-
-  sref = l;
-  cout << "sref.x: " << sref.x << "   l.x: " << l.x << endl;
-  cout << "address of sref: " << &sref << "  address of l: " << &l << endl;
-  cout << "sref.get_x(): " << sref.get_x() << "   l.x: " << l.x << endl;
-  sref.draw();
+  s -> draw();
+  s = &cb;
+  s -> draw();
 }
