3ff46a4d6f8c4ce5b45b60e9f61e00360675d4bc
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-06-09 17:06:03 +0200

    verify fermat's last theorem with a python script

--- algorithm_drills/fermat_last.py
+++ algorithm_drills/fermat_last.py
@@ -1,12 +1,18 @@
-#comment
-x = 1
-y = 1
-total = 5
+#verify Fermat's Last Theorem
 
-while x <= total - 2:
+n = 3  #set n = 2 to generate pythagorean triples
+       #no triple will be found for n > 2
+total = 3
+
+while total < 1024:
+  x = 1
   y = 1
-  while y < total - x:
-    z = total - y - x
-    print(x, y, z)
-    y += 1
-  x += 1
+  while x <= total - 2:
+    y = 1
+    while y < total - x:
+      z = total - y - x
+      if x ** n + y ** n == z ** n:
+        print('found triple:', x, y, z)
+      y += 1
+    x += 1
+  total += 1
