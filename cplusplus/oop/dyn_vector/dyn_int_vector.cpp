f7d148b7fe9379cd800883500b3f6eb8dbdcac1b
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-06-15 11:20:43 +0200

    fix spaces

--- cplusplus/oop/dyn_vector/dyn_int_vector.cpp
+++ cplusplus/oop/dyn_vector/dyn_int_vector.cpp
@@ -1,13 +1,13 @@
 #include "dyn_int_vector.h"
 
 dyn_int_vector::dyn_int_vector(int size, int val) {
-  this->size = size;
+  this -> size = size;
   ptr = new int[size];
   for (int i = 0; i < size; i++) {
-    ptr[i]= val;
+    ptr[i] = val;
   }
 }
 
-dyn_int_vector:: ~dyn_int_vector() {
+dyn_int_vector :: ~dyn_int_vector() {
   delete[] ptr;
 }
