f85b86ab1cb2c6c07ed841596b6a427aedca2e9b
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-08-10 18:32:37 +0200

    fixes

--- networks_tlc/sockets/tanenbaum_fileserver/client.c
+++ networks_tlc/sockets/tanenbaum_fileserver/client.c
@@ -11,7 +11,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-#define SERVER_PORT 8080	/* arbitrary, but client & server must agree */
+#define SERVER_PORT 12345	/* arbitrary, but client & server must agree */
 #define BUF_SIZE 4096	/* block transfer size */
 
 int main(int argc, char **argv)
@@ -21,18 +21,29 @@ int main(int argc, char **argv)
   struct hostent *h;	/* info about server */
   struct sockaddr_in channel;	/* holds IP address */
 
-  if (argc != 3) {printf("Usage: client server-name file-name\n"); exit (-1);}
+  if (argc != 3) {
+    printf("Usage: client server-name file-name\n"); exit (-1);
+  }
   h = gethostbyname(argv[1]);	/* look up host's IP address */
-  if (!h) {printf("gethostbyname failed to locate %s\n", argv[1]); exit (-1);}
+  if (!h) {
+    printf("gethostbyname failed to locate %s\n", argv[1]);
+    exit (-1);
+  }
 
   s = socket(PF_INET, SOCK_STREAM, IPPROTO_TCP);
-  if (s < 0) {printf("socket called failed\n"); exit (-1);}
+  if (s < 0) {
+    printf("socket called failed\n");
+    exit (-1);
+  }
   memset(&channel, 0, sizeof(channel));
   channel.sin_family= AF_INET;
   memcpy(&channel.sin_addr.s_addr, h->h_addr, h->h_length);
   channel.sin_port= htons(SERVER_PORT);
   c = connect(s, (struct sockaddr *) &channel, sizeof(channel));
-  if (c < 0) {printf("connect failed\n"); exit (-1);}
+  if (c < 0) {
+    printf("connect failed\n");
+    exit (-1);
+  }
 
   /* Connection is now established. Send file name including 0 byte at end. */
   write(s, argv[2], strlen(argv[2])+1);
--- networks_tlc/sockets/tanenbaum_fileserver/server.c
+++ networks_tlc/sockets/tanenbaum_fileserver/server.c
@@ -8,7 +8,7 @@
 #include <netinet/in.h>
 #include <netdb.h>
 
-#define SERVER_PORT 8080	/* arbitrary, but client & server must agree */
+#define SERVER_PORT 12345	/* arbitrary, but client & server must agree */
 #define BUF_SIZE 4096	/* block transfer size */
 #define QUEUE_SIZE 10
 
@@ -26,25 +26,39 @@ int main(int argc, char *argv[])
 
   /* Passive open. Wait for connection. */
   s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);    /* create socket */
-  if (s < 0) {printf("socket failed\n"); exit(-1);}
+  if (s < 0) {
+    printf("socket failed\n"); exit(-1);
+  }
   setsockopt(s, SOL_SOCKET, SO_REUSEADDR, (char *) &on, sizeof(on));
 
   b = bind(s, (struct sockaddr *) &channel, sizeof(channel));
-  if (b < 0) {printf("bind failed\n"); exit(-1);}
+  if (b < 0) {
+    printf("bind failed\n");
+    exit(-1);
+  }
 
   l = listen(s, QUEUE_SIZE);	/* specify queue size */
-  if (l < 0) {printf("listen failed\n"); exit(-1);}
+  if (l < 0) {
+    printf("listen failed\n");
+    exit(-1);
+  }
 
   /* Socket is now set up and bound. Wait for connection and process it. */
   while (1) {
         sa = accept(s, 0, 0);	/* block for connection request */
-        if (sa < 0) {printf("accept failed\n"); exit(-1);}
+        if (sa < 0) {
+          printf("accept failed\n");
+          exit(-1);
+        }
 
         read(sa, buf, BUF_SIZE);	/* read file name from socket */
 
         /* Get and return the file. */
         fd = open(buf, O_RDONLY);	/* open the file to be sent back */
-        if (fd < 0) {printf("open failed\n"); exit(-1);}
+        if (fd < 0) {
+          printf("open failed\n");
+          exit(-1);
+        }
 
         while (1) {
                 bytes = read(fd, buf, BUF_SIZE); /* read from file */
