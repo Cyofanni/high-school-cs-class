289ed44fed8b65cd25d6ddb597d3f2cc37e4c231
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-08-30 20:07:15 +0200

    improve cdma simulation

--- networks_tlc/cdma.py
+++ networks_tlc/cdma.py
@@ -25,18 +25,66 @@ print(norm_inner_prod(ST2_chipcode, ST3_chipcode))
 print(norm_inner_prod(ST2_chipcode, ST4_chipcode))
 print(norm_inner_prod(ST3_chipcode, ST4_chipcode))
 
-#ST3 transmits the bit sequence: 1 1 silent 1 1 - 1
-signal_1 = ST3_chipcode                    #ST3 transmits its own chipcode (alone)
-signal_2 = ST2_chipcode + ST3_chipcode     #ST3 transmits its own chipcode
-signal_3 = ST1_chipcode + negate_chipcode(ST2_chipcode) #ST3 is silent
-signal_4 = ST1_chipcode + negate_chipcode(ST2_chipcode) + ST3_chipcode  #ST3 transmits its own chipcode
-signal_5 = ST1_chipcode + ST2_chipcode + ST3_chipcode + ST4_chipcode    #ST3 transmits its own chipcode
-signal_6 = ST1_chipcode + ST2_chipcode + negate_chipcode(ST3_chipcode) + ST4_chipcode  #ST3 transmits its own chipcode, negated
+#S: station is silent
+#ST1 transmits the bit sequence: S S 1 1 1 1 1 0
+#ST2 transmits the bit sequence: S 1 0 0 1 1 1 S
+#ST3 transmits the bit sequence: 1 1 S 1 1 0 1 1
+#ST4 transmits the bit sequence: S S S S 1 1 1 0
 
+signal_1 = ST3_chipcode #ST3 transmits its own chipcode (alone)
+signal_2 = ST2_chipcode + ST3_chipcode #ST3 transmits its own chipcode
+signal_3 = ST1_chipcode + negate_chipcode(ST2_chipcode) #ST3 is silent
+signal_4 = ST1_chipcode + negate_chipcode(ST2_chipcode) + \
+           ST3_chipcode #ST3 transmits its own chipcode
+signal_5 = ST1_chipcode + ST2_chipcode + ST3_chipcode + \
+           ST4_chipcode #ST3 transmits its own chipcode
+signal_6 = ST1_chipcode + ST2_chipcode + \
+           negate_chipcode(ST3_chipcode) + ST4_chipcode #ST3 transmits its own chipcode, negated
+signal_7 = ST1_chipcode + ST2_chipcode + \
+           ST3_chipcode + ST4_chipcode
+signal_8 = negate_chipcode(ST1_chipcode) + ST3_chipcode + \
+           + negate_chipcode(ST4_chipcode)
+
+print()
+print("extracted ST1's bits")
+print(norm_inner_prod(signal_1, ST1_chipcode), end = ' ')
+print(norm_inner_prod(signal_2, ST1_chipcode), end = ' ')
+print(norm_inner_prod(signal_3, ST1_chipcode), end = ' ')
+print(norm_inner_prod(signal_4, ST1_chipcode), end = ' ')
+print(norm_inner_prod(signal_5, ST1_chipcode), end = ' ')
+print(norm_inner_prod(signal_6, ST1_chipcode), end = ' ')
+print(norm_inner_prod(signal_7, ST1_chipcode), end = ' ')
+print(norm_inner_prod(signal_8, ST1_chipcode), end = '\n')
+
+print()
+print("extracted ST2's bits")
+print(norm_inner_prod(signal_1, ST2_chipcode), end = ' ')
+print(norm_inner_prod(signal_2, ST2_chipcode), end = ' ')
+print(norm_inner_prod(signal_3, ST2_chipcode), end = ' ')
+print(norm_inner_prod(signal_4, ST2_chipcode), end = ' ')
+print(norm_inner_prod(signal_5, ST2_chipcode), end = ' ')
+print(norm_inner_prod(signal_6, ST2_chipcode), end = ' ')
+print(norm_inner_prod(signal_7, ST2_chipcode), end = ' ')
+print(norm_inner_prod(signal_8, ST2_chipcode), end = '\n')
+
+print()
 print("extracted ST3's bits")
-print(norm_inner_prod(signal_1, ST3_chipcode))
-print(norm_inner_prod(signal_2, ST3_chipcode))
-print(norm_inner_prod(signal_3, ST3_chipcode))
-print(norm_inner_prod(signal_4, ST3_chipcode))
-print(norm_inner_prod(signal_5, ST3_chipcode))
-print(norm_inner_prod(signal_6, ST3_chipcode))
+print(norm_inner_prod(signal_1, ST3_chipcode), end = ' ')
+print(norm_inner_prod(signal_2, ST3_chipcode), end = ' ')
+print(norm_inner_prod(signal_3, ST3_chipcode), end = ' ')
+print(norm_inner_prod(signal_4, ST3_chipcode), end = ' ')
+print(norm_inner_prod(signal_5, ST3_chipcode), end = ' ')
+print(norm_inner_prod(signal_6, ST3_chipcode), end = ' ')
+print(norm_inner_prod(signal_7, ST3_chipcode), end = ' ')
+print(norm_inner_prod(signal_8, ST3_chipcode), end = '\n')
+
+print()
+print("extracted ST4's bits")
+print(norm_inner_prod(signal_1, ST4_chipcode), end = ' ')
+print(norm_inner_prod(signal_2, ST4_chipcode), end = ' ')
+print(norm_inner_prod(signal_3, ST4_chipcode), end = ' ')
+print(norm_inner_prod(signal_4, ST4_chipcode), end = ' ')
+print(norm_inner_prod(signal_5, ST4_chipcode), end = ' ')
+print(norm_inner_prod(signal_6, ST4_chipcode), end = ' ')
+print(norm_inner_prod(signal_7, ST4_chipcode), end = ' ')
+print(norm_inner_prod(signal_8, ST4_chipcode), end = '\n')
