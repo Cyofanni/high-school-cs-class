d74f119da824a2fd955d3269ae464b382e03e5d6
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-11-09 08:44:09 +0100

    remove folder

--- numeric_algos/linear_algebra/matrices.h
+++
@@ -1,7 +0,0 @@
-void print_matrix(double mat[], const int& n_rows,
-			  const int& n_cols);
-
-void matrix_mult(double matr1[4][6], double matr2[6][5],
-		 double matr3[4][5]);
-
-void gaussian_elimination(double system[3][4]);
--- numeric_algos/linear_algebra/solve_lin_sys.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-
-A = np.array([[6, 4, -1], [-3, -1, 2],
-              [-2, 1, 2]])
-
-b = np.array([12, -11, 8])
-
-print(np.linalg.solve(A, b))
