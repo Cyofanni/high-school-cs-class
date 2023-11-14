e57c0378dd25f472456995ed35fef01278165edc
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-11-14 21:20:32 +0100

    add exercises on turtle graphics

---
+++ python/turtle_graphics/exercises.py
@@ -0,0 +1,18 @@
+from turtle import *
+
+def ex1():
+    left(60)
+    for i in range(4):
+        forward(100)
+        right(120)
+        forward(100)
+        left(120)
+
+def ex2():
+    for i in range(6):
+        left(60)
+        forward(100)
+        right(120)
+        forward(100)
+
+mainloop()
