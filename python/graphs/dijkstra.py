53b1079521d155feb75e1ff0fe4becf782e3b025
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-11-16 14:22:30 +0100

    fixes

--- python/graphs/dijkstra.py
+++ python/graphs/dijkstra.py
@@ -6,16 +6,16 @@ def extract_min(d):
   return min_v
 
 def dijkstra(g, g_c, g_e, p):
-  Q = G_costs.copy()
+  Q = g_c.copy()
   while len(Q) > 0:
     u = extract_min(Q)
-    u_successors = G[u]
+    u_successors = g[u]
     for v in u_successors:
       edge_name = u + v
       edge_cost = g_e[edge_name]
-      if G_costs[v] > G_costs[u] + edge_cost:
-        G_costs[v] = G_costs[u] + edge_cost
-        Q[v] = G_costs[u] + edge_cost
+      if g_c[v] > g_c[u] + edge_cost:
+        g_c[v] = g_c[u] + edge_cost
+        Q[v] = g_c[u] + edge_cost
         p[v] = u
 
 def print_path_rec(p, v):
@@ -68,6 +68,42 @@ P = {
       'y': None
     }
 
+G_1 = {
+      's': ['r','t','x'],
+      'r': ['t'],
+      't': ['y','x','s'],
+      'x': ['y'],
+      'y': []
+    }
+
+G_costs_1 = {
+            's': 0,
+            'r': math.inf,
+            't': math.inf,
+            'x': math.inf,
+            'y': math.inf
+          }
+
+G_edges_1 = {
+            'sr': 5,
+            'st': 6,
+            'sx': 3,
+            'rt': 2,
+            'tx': 1,
+            'ty': 3,
+            'tx': 1,
+            'xy': 9,
+            'ts': -10
+          }
+
+P_1 = {
+      's': None,
+      'r': None,
+      't': None,
+      'x': None,
+      'y': None
+    }
+
 dijkstra(G, G_costs, G_edges, P)
 print(G_costs)
 print(P)
@@ -75,3 +111,5 @@ print_path_rec(P, 'y')
 print()
 print_path_rec(P, 't')
 print()
+dijkstra(G_1, G_costs_1, G_edges_1, P_1)
+print(G_costs_1)
