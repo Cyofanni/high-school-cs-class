cf9e1c8b75e43acdaa158a12fd51c2938c0adc24
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-11-16 13:47:00 +0100

    fix dijkstra

--- python/graphs/dijkstra.py
+++ python/graphs/dijkstra.py
@@ -1,71 +1,67 @@
-#Dijkstra’s algorithm
-
 import math
-
-def print_costs_sp_src(graph, source):
-  for key in graph.keys():
-    print('Cost of shortest path with source', source + ', dest', key + ':',
-          str(graph[key]['cst_sp_src']) + ' (computed so far)')
 
 def extract_min(d):
   min_v = min(d, key = d.get)
   d.pop(min_v)
   return min_v
 
-def dijkstra(G, source, preds):
-  Q = {}
-  for v in G.keys():
-    Q[v] = G[v]['cst_sp_src']
-
+def dijkstra(g, g_c, g_e, p):
+  Q = G_costs.copy()
   while len(Q) > 0:
-    v = extract_min(Q)
-    v_succs = G[v].copy()
-    v_succs.pop('cst_sp_src')
+    u = extract_min(Q)
+    u_successors = G[u]
+    for v in u_successors:
+      edge_name = u + v
+      edge_cost = g_e[edge_name]
+      if G_costs[v] > G_costs[u] + edge_cost:
+        G_costs[v] = G_costs[u] + edge_cost
+        Q[v] = G_costs[u] + edge_cost
+        p[v] = u
 
-    for u in v_succs:
-      uv_weight = G[v][u]
-      v_c = G[v]['cst_sp_src']
-      u_c = G[u]['cst_sp_src']
-      new_cst_sp_src = v_c + uv_weight
-      if v_c + uv_weight < u_c:
-        G[u]['cst_sp_src'] = new_cst_sp_src
-        preds[u] = v
-        Q[u] = new_cst_sp_src
+def print_path_rec(p, v):
+  if p[v] == None:
+    print(v, end = ' ')
+    return
+  print_path_rec(p, p[v])
+  print(v, end = ' ')
 
-    print_costs_sp_src(G, 's')
-    cont = input()
-    print("*************************")
+def print_path_iter(p, v):
+  path = []
+  while p[v] != None:
+    path.insert(0, v)
+    v = p[v]
+  path.insert(0, v)
+  print(path)
 
-source = 's'
-G = {
-      's':{'r':5,'t':6,'x':3,'cst_sp_src':0},
-      'y':{'cst_sp_src':math.inf},
-      'r':{'t':2,'cst_sp_src':math.inf},
-      't':{'y':3,'x':1,'cst_sp_src':math.inf},
-      'x':{'y':9,'cst_sp_src':math.inf}
+G = {'s':['t','y'],
+     'y':['x','z','t'],
+     'z':['x','s'],
+     't':['x','y'],
+     'x':['z']
     }
 
-pred_sp_src = {'s':None,'y':None,'r':None,'t':None,'x':None}
+G_costs = {'s':0,
+           't':math.inf,
+           'x':math.inf,
+           'z':math.inf,
+           'y':math.inf
+          }
 
-source1 = 's'
-G1 = {
-      's':{'t':10,'y':5,'cst_sp_src':0},
-      't':{'x':1,'y':2,'cst_sp_src':math.inf},
-      'y':{'t':3,'x':9,'z':2,'cst_sp_src':math.inf},
-      'z':{'s':7,'x':6,'cst_sp_src':math.inf},
-      'x':{'z':4,'cst_sp_src':math.inf}
-     }
+G_edges = {'st':10,
+           'sy':5,
+           'yx':9,
+           'yz':2,
+           'yt':3,
+           'zx':6,
+           'zs':7,
+           'tx':1,
+           'ty':2,
+           'xz':4
+          }
 
-pred_sp_src1 = {'s':None,'t':None,'y':None,'x':None,'z':None}
-
-dijkstra(G1, source1, pred_sp_src1)
-
-for dest in G1.keys():
-  dest_v = dest
-  shortest_path = [dest_v]
-  while dest_v != None:
-    dest_v = pred_sp_src1[dest_v]
-    if (dest_v != None):
-      shortest_path.insert(0, dest_v)
-
-  print('Shortest path', source1, '->', dest, 'is:', shortest_path)
+P = {'s':None,
+     't':None,
+     'x':None,
+     'y':None,
+     'z':None
+    }
