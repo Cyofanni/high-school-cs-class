3b0c4740e1495780ab7a3db188c091ce947dc31a
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-12-25 21:59:26 +0100

    add gaussian elimination

---
+++ numerical_analysis/gaussian_elimination.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  //const int n_rows = 3, n_cols = 4;
+  const int n_rows = 4, n_cols = 5;
+  /*double A_b[n_rows][n_cols] = {{6,4,-1,12},
+			       {-3,-1,2,-11},
+			       {-2,1,2,8}};*/
+  /*double A_b[n_rows][n_cols] = {{2,1,-1,8},
+			       {-3,-1,2,-11},
+			       {-2,1,2,-3}};
+  */
+  /*double A_b[n_rows][n_cols] = {{2,1,-1,6,8},
+				{4,-3,-1,2,-11},
+				{-2,1,2,8,-3},
+				{6,6,2,8.2,-3}};
+  */
+  double A_b[n_rows][n_cols] = {{2,13,-13,2,3},
+				{3,-3,-1,2,11},
+				{-2,1,2,1,-3},
+				{3,12,2,8.2,-15}};
+
+  //loop over diagonal items
+  for (int i = 0; i <= n_rows - 2; i++) {
+    double u = A_b[i][i];
+    //u could be 0, or very small (don't care for this example)
+    //loop over item's column (below the item)
+    for (int j = i + 1; j <= n_rows - 1; j++) {
+      double l = A_b[j][i] / u;
+      //loop on the row
+      for (int k = i; k <= n_cols - 1; k++) {
+	A_b[j][k] = A_b[j][k] - l * A_b[i][k];
+      }
+    }
+  }
+  for (int i = 0; i < n_rows; i++) {
+    for (int j = 0; j < n_cols; j++) {
+      cout << A_b[i][j] << ' ';
+    }
+    cout << endl;
+  }
+  cout << endl;
+  //back substitution
+  double x[n_rows];
+  for (int i = n_rows - 1; i >= 0; i--) {
+    double b = A_b[i][n_cols - 1];
+    double sum = 0;
+    for (int j = i + 1; j <= n_cols - 2; j++) {
+      sum = sum + A_b[i][j] * x[j];
+    }
+    x[i] = (b - sum) / A_b[i][i];
+  }
+  for (int i = 0; i < n_rows; i++) {
+    cout << x[i] << ' ';
+  }
+  cout << endl;
+}
---
+++ numerical_analysis/numpy_matrices.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+A = np.array([[6, 4, -1],
+              [-3, -1, 2],
+              [-2, 1, 2]])
+b = np.array([12, -11, 8])
+
+A1 = np.array([[2, 1, -1],
+              [-3, -1, 2],
+              [-2, 1, 2]])
+b1 = np.array([8, -11, -3])
+
+A2 = np.array([[2, 1, -1, 6],
+               [4, -3, -1, 2],
+               [-2, 1, 2, 8],
+               [6, 6, 2, 8.2]])
+b2 = np.array([8, -11, -3, -3])
+
+A3 = np.array([[2, 13, -13, 2],
+               [3, -3, -1, 2],
+               [-2, 1, 2, 1],
+               [3, 12, 2, 8.2]])
+b3 = np.array([3, 11, -3, -15])
+
+print(np.linalg.solve(A, b))
+print(np.linalg.solve(A1, b1))
+print(np.linalg.solve(A2, b2))
+print(np.linalg.solve(A3, b3))
