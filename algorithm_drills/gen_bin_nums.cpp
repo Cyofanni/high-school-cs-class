257967d93ace7cab5b4ca8797ce3564cc3b90d84
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-12-27 20:21:32 +0100

    generate all binary numbers recursively

---
+++ algorithm_drills/gen_bin_nums.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector<vector<int>> numbers;
+
+void gen_bin_numbers(int n, vector<int> number) {
+  if (n == 0) {
+    numbers.push_back(number);
+    return;
+  }
+  vector<int> number_0 = number;
+  number_0.push_back(0);
+  gen_bin_numbers(n - 1, number_0);
+  vector<int> number_1 = number;
+  number_1.push_back(1);
+  gen_bin_numbers(n - 1, number_1);
+}
+
+int main(int argc, char* argv[]) {
+  vector<int> num;
+  gen_bin_numbers(5, {});
+  for (auto num : numbers) {
+    for (auto i : num) {
+      cout << i;
+    }
+    cout << endl;
+  }
+}
