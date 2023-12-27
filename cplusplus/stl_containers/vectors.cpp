f523152b3a00cf23acf6f6fd0a8367c8bb9be8c1
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2023-12-27 16:08:56 +0100

    add examples on vectors

--- cplusplus/stl_containers/vectors.cpp
+++ cplusplus/stl_containers/vectors.cpp
@@ -5,16 +5,17 @@
 using namespace std;
 
 int main() {
+  //example 0
   vector<int> v(16);
   for (int item : v) {
     cout << item << ' ';
   }
-
   cout << endl;
-
   //out_of_range
   //cout << v.at(16) << endl;
 
+  //example 1
+  cout << endl;
   vector<double> nums1 = {5, 6, 5, 8, 7, 13, 15, 98, 78, 25, 14, 1};
   vector<double> nums2 = nums1; //copy, item by item
   nums2.at(0) = 90892;
@@ -27,6 +28,8 @@ int main() {
   }
   cout << endl;
 
+  //example 2
+  cout << endl;
   vector<int> grades;
   int n = 0;
   while (n != -1) {
@@ -37,8 +40,9 @@ int main() {
   for (auto grade : grades) {
     cout << grade << ' ';
   }
-  cout << endl;
 
+  //example 3
+  cout << endl;
   try {
     vector<int> v1(9998988776713877211);
   }
@@ -46,10 +50,24 @@ int main() {
     cout << "vector max size exceeded" << endl;
   }
 
+  //example 4 (iterator)
+  cout << endl;
   vector<int> vec1 = {6, 8, 7, 1, 6, 7};
   vector<int>::iterator it;
-  it = find(vec1.begin(), vec1.end(), 8);
-  if (it) {
+  it = find(vec1.begin(), vec1.end(), 7);
+  if (it != vec1.end()) {
     cout << *it << endl;
+  }
+
+  //example 5 (matrix)
+  vector<vector<double>> matrix = {{4, 5, 3},
+                                   {1, 2, 3},
+                                   {6, 5, 4}};
+  cout << endl;
+  for (auto row : matrix) {
+    for (auto item : row) {
+       cout << item << ' ';
+    }
+    cout << endl;
   }
 }
