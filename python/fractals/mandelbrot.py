be968b13a5aa1a2fc3d8beeabc667725419f21de
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-01-10 21:44:08 +0100

    fix

--- python/fractals/mandelbrot.py
+++ python/fractals/mandelbrot.py
@@ -1,30 +1,33 @@
 from matplotlib import pyplot
+import time
 
 def is_point_of_M(c, num_iters):
     z = 0
-    i = 0
-    diverged = False
-    while i < num_iters and diverged == False:
+    i = 1
+    while i <= num_iters:
         z = z ** 2 + c
-        if abs(z - c) > 10 ** 8:
-            diverged = True
+        if abs(z - c) > 2:
+            return True
         i += 1
 
-    return not diverged
+    return False
 
-#x:[-2, 2], y:[-3, 3], step:0.1, num_iters: 16 --> 2s
-#x:[-2, 2], y:[-3, 3], step:0.01, num_iters: 16 --> 16m32s
+x = -1.5
+final_x = 0.5
+initial_y = -1
+final_y = 1
+step = 0.01
+num_iters = 20
 
-x = -2
-final_x = -x
-initial_y = -3
-final_y = -initial_y
-step = 0.1
-num_iters = 16
+start_time = time.time()
 while x <= final_x:
   y = initial_y
   while y <= final_y:
     if is_point_of_M(complex(x, y), num_iters) == True:
-      pyplot.scatter([x], [y], [3], color = 'black')
+      pyplot.scatter([x], [y], [1], color = 'black')
     y = y + step
   x = x + step
+
+pyplot.show()
+
+print(time.time() - start_time)
