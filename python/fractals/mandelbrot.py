727e17c97815d721fb2896027655161831482e7a
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-02-15 10:51:13 +0100

    fix

--- python/fractals/mandelbrot.py
+++ python/fractals/mandelbrot.py
@@ -1,34 +1,26 @@
 from matplotlib import pyplot
-import time
 
-def is_point_of_M(c, num_iters):
-    z = 0
-    i = 1
-    while i <= num_iters:
-        z = z ** 2 + c
-        if abs(z - c) > 2:
-            return False
+def is_in_mandelbrot(c):
+  z = 0
+  for i in range(20):
+    z = z ** 2 + c
+    if abs(z - c) > 2:
+      return False
+  return True
 
-        i += 1
+def draw_mandelbrot_escape_time(step):
+  x_init = -2
+  x_final = 0.5
+  y_init = -1.2
+  y_final = 1.2
 
-    return True
+  x = x_init
+  while x <= x_final:
+    y = y_init
+    while y <= y_final:
+      if is_in_mandelbrot(complex(x, y)) == True:
+        pyplot.scatter([x], [y], 1, color = 'black')
+      y += step
+    x += step
 
-x = -1.5
-final_x = 0.5
-initial_y = -1
-final_y = 1
-step = 0.01
-num_iters = 20
-
-start_time = time.time()
-while x <= final_x:
-  y = initial_y
-  while y <= final_y:
-    if is_point_of_M(complex(x, y), num_iters) == True:
-      pyplot.scatter([x], [y], [1], color = 'black')
-    y = y + step
-  x = x + step
-
-pyplot.show()
-
-print(time.time() - start_time)
+draw_mandelbrot_escape_time(0.01)
