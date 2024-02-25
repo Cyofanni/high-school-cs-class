296da170e57d1ea93dabc1b059072e4502518e1e
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-02-25 20:03:22 +0100

    add differential equation

--- numerical_analysis/euler_method.py
+++ numerical_analysis/euler_method.py
@@ -3,30 +3,39 @@ from sympy import *
 from sympy.abc import x
 import math
 
-#solve the ODE algebraically
+#solve ODE's algebraically
 f = Function('f')
-result = dsolve(diff(f(x), x) - f(x) + x ** 2 - 1, f(x), ics={f(0): 0.5})
-print('known solution:', result)
+re1 = dsolve(diff(f(x), x) - f(x) + x ** 2 - 1, f(x), ics = {f(0): 0.5})
+print('known solution:', re1)
+print()
+re2 = dsolve(diff(f(x), x) - 2 * f(x), f(x), ics = {f(0): 10})
+print('known solution:', re2)
 
 #euler method
 
-def f(x, y):
+def f1(x, y):
   return y - x ** 2 + 1
 
-#compare numerical result with known solution
-def solution(x):
+def f2(x, y):
+  return 2 * y
+
+#compare numerical result with known solutions
+def solution1(x):
   return (x + 1) ** 2 - 0.5 * math.exp(x)
 
-def euler_method(a, b, n, w, f):
-  output = []
+def solution2(x):
+  return 10 * exp(2 * x)
+
+def euler_method(f, a, b, n, w):
+  output = [(a, w)]
   step = (b - a) / n
   x = a
-  print('approximation: x:', x, '    f(x):', w)
-  print('solution: x:', x, '    f(x):', solution(x))
+  #print('approximation: x:', x, '    f(x):', w)
+  #print('solution: x:', x, '    f(x):', solution(x))
   print()
   for i in range(1, n + 1):
     w = w + step * f(x, w)
-    output.append((x, w))
+    output.append((x + step, w))
     x = a + i * step
     #print('approximation: x:', x, '    f(x):', w)
     #print('solution: x:', x, '    f(x):', solution(x))
@@ -34,6 +43,6 @@ def euler_method(a, b, n, w, f):
 
   return output
 
-res = euler_method(0, 4, 10, 0.5, f)
+res = euler_method(f2, 0, 3, 3, 10)
 for item in res:
   print(item)
