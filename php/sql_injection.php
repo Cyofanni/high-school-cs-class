6d3fbcc7e9823b664aece167566a75df8ab0bdbd
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-04-02 17:08:11 +0200

    fix

--- php/sql_injection.php
+++ php/sql_injection.php
@@ -6,7 +6,6 @@
     }
 
     echo "enter an id: ";
-    $id_input = 0;
     $id_input = fgets(STDIN);
     $sql_query = "SELECT * FROM doctors WHERE id = " . $id_input;
     $results = $db->query($sql_query);
