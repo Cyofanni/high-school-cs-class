789550c4f8ebe126d30b12133e515c53b6aa8eac
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-05-10 21:11:23 +0200

    improve code

--- C/hacking/stack_buf_overflow_basic.c
+++ C/hacking/stack_buf_overflow_basic.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 
-/* compilation command:
-     gcc -fno-stack-protector -o stack_buf_overflow_basic stack_buf_overflow_basic.c
-*/
+//gcc -fno-stack-protector -o stack_buf_overflow_basic stack_buf_overflow_basic.c
+
 int main(int argc, char* argv[]) {
     unsigned short int i = 0xFF10;
     char buffer[8] = {};
