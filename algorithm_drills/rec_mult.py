0442a57030f183787ea265860fe6bd7ff1f14778
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-05-28 15:48:08 +0200

    fix

--- algorithm_drills/rec_mult.py
+++ algorithm_drills/rec_mult.py
@@ -2,8 +2,8 @@ def mult(a, b):
   if a == 0:
     return 0
   if a % 2 == 0:
-    return (a // 2) * 2 * b
-  return (a // 2) * 2 * b + b
+    return mult((a // 2), 2 * b)
+  return mult((a // 2) * 2, b) + b
 
 while True:
   p = int(input('enter a natural number: '))
