c2abc262f282add4e9ff15d5a11b29c355cd3600
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-05-30 08:46:28 +0200

    fix

--- algorithm_drills/integer_mult.py
+++ algorithm_drills/integer_mult.py
@@ -2,19 +2,19 @@
 def mr(x, y, n):
     if n <= 1:
         return x * y
-    a = x//(10**(n/2))
-    b = x%(10**(n/2))
-    c = y//(10**(n/2))
-    d = y%(10**(n/2))
+    a = x//(10**(n//2))
+    b = x%(10**(n//2))
+    c = y//(10**(n//2))
+    d = y%(10**(n//2))
     return (10**n)*mr(a,c,n//2)+(10**(n//2))*(mr(a,d,n//2)+mr(b,c,n//2))+mr(b,d,n//2)
 
 def karatsuba(x, y, n):
     if n <= 1:
         return x * y
-    a = x//(10**(n/2))
-    b = x%(10**(n/2))
-    c = y//(10**(n/2))
-    d = y%(10**(n/2))
+    a = x//(10**(n//2))
+    b = x%(10**(n//2))
+    c = y//(10**(n//2))
+    d = y%(10**(n//2))
     ac = karatsuba(a, c, n//2)
     bd = karatsuba(b, d, n//2)
     k1 = a + b
@@ -23,10 +23,11 @@ def karatsuba(x, y, n):
     return (10**n)*ac + (10**(n//2))*(k3-ac-bd) + bd
 
 n = 16
-x = 2000_7653
-y = 4231_5463
+x = 34246454232
+y = 1151534534987394858762
 r1 = mr(x, y, n)
 r2 = karatsuba(x, y, n)
-r3 = x * y
-print(r1, r2, r3)
-print(r1 == r2 == r3)
+r3 = int(x) * int(y)
+print(int(r1), int(r2), int(r3))
+print(r1 == r2)
+print(r2 == r3)
