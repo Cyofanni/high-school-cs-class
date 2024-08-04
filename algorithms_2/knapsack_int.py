b09955dff417997525dcb177ad2a64df5945c0ef
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-08-04 17:07:51 +0200

    fix knapsack

--- algorithms_2/knapsack_int.py
+++ algorithms_2/knapsack_int.py
@@ -4,27 +4,24 @@ def knapsack_int(items, i, weight):
     if weight <= 0 or i == len(items):
         return 0
 
-    res = -math.inf
-    for k in range(i, len(items)):
-        if items[k][1] <= weight:
-            new_weight = weight - items[k][1]
-            v1 = items[k][0] + knapsack_int(items, k + 1, new_weight)
-        else:
-            v1 = -math.inf
-        v2 = knapsack_int(items, k + 1, weight)
-        v3 = max(v1, v2)
-        if v3 > res:
-            res = v3
+    if items[i][1] <= weight:
+        new_weight = weight - items[i][1]
+        v1 = items[i][0] + knapsack_int(items, i + 1, new_weight)
+    else:
+        v1 = -math.inf
+    v2 = knapsack_int(items, i + 1, weight)
 
-    return res
+    if v1 >= v2:
+        return v1
+    return v2
 
 #(value, weight)
 items = [(60,10),(100,20),(120,30)]
-res = knapsack_int(items, 0, 50)
+res = knapsack_int(items, 0, 80)
 print(res)
 print()
 
-items = [(1,4),(2,5),(3,1)]
-res = knapsack_int(items, 0, 1)
+items = [(1,4),(2,5),(3,6)]
+res = knapsack_int(items, 0, 3)
 print(res)
 print()
