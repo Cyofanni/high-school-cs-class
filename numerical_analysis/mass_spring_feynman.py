65ce37968216db2f16eca56ff0aa7e6c9911e929
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-08-16 18:33:58 +0200

    add mass on a spring (feynman lectures on physics)

---
+++ numerical_analysis/mass_spring_feynman.py
@@ -0,0 +1,22 @@
+#the feynman lectures on physics: vol. 1, chapter 9-5
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+num_pts = 1500
+xs = [0] * num_pts
+xs[0] = 1
+vs = [0] * num_pts
+h = 0.01
+
+for t in range(1, num_pts):
+    vs[t] = vs[t - 1] - h * xs[t - 1]
+    xs[t] = xs[t - 1] + h * vs[t - 1]
+
+for i in range(num_pts):
+    print(str(h * i) + '  ' + str(xs[i]) + '  ' + str(vs[i]) + '  ' + str(-xs[i]))
+
+ts = [h * t for t in range(num_pts)]
+plt.plot(ts, xs)
+plt.plot(ts, np.cos(ts))
+plt.show()
