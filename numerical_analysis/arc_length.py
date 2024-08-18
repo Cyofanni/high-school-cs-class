7534cdecf863b517066ffa9c83e99cfa027eab85
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-08-18 21:11:20 +0200

    compute arc length numerically

---
+++ numerical_analysis/arc_length.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+def f0(x):
+  return np.sin(x) + np.exp(x)
+
+def arc_length_numerical(f, a, b, n):
+  dx = (b - a) / n
+  xs = [i * dx for i in range(n + 1)]
+  ys = [f(x) for x in xs]
+  ls = [np.sqrt((xs[i]-xs[i-1])**2 + (ys[i]-ys[i-1])**2) for i in range(1, n + 1)]
+  return sum(ls)
+
+print(arc_length_numerical(f0, 0, 6, 100))
