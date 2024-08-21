125a67009c5bbb3361a186813c5f3567bca82da6
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-08-21 16:31:32 +0200

    find paths in maze

--- algorithm_drills/maze.py
+++ algorithm_drills/maze.py
@@ -1,6 +1,6 @@
 import random
 
-#recursive calls only on free and white slots
+#recursive call only on free slots
 def solve(mz, row, col, n_rows, n_cols):
     if row == n_rows - 1:
         return True
@@ -9,20 +9,29 @@ def solve(mz, row, col, n_rows, n_cols):
        and mz[row][col + 1][0] == 'X':
             return False
 
+    #colour gray
     mz[row][col][1] = 'G'
     left, down, right = False, False, False
     #left
     if mz[row][col - 1][1] == 'W' and mz[row][col - 1][0] == '_':
+        mz[row][col - 1][0] = '*'
         left = solve(mz, row, col - 1, n_rows, n_cols)
-
+        if left == False:
+            mz[row][col - 1][0] = '_'
     #down
     if mz[row + 1][col][1] == 'W' and mz[row + 1][col][0] == '_':
+        mz[row + 1][col][0] = '*'
         down = solve(mz, row + 1, col, n_rows, n_cols)
-
+        if down == False:
+            mz[row + 1][col][0] = '_'
     #right
     if mz[row][col + 1][1] == 'W' and mz[row][col + 1][0] == '_':
+        mz[row][col + 1][0] = '*'
         right = solve(mz, row, col + 1, n_rows, n_cols)
+        if right == False:
+            mz[row][col + 1][0] = '_'
 
+    #colour black
     mz[row][col][1] = 'B'
     return left or down or right
 
@@ -30,11 +39,11 @@ def generate_maze(n_rows, n_cols):
     mz = [[None for _ in range(n_cols)] for _ in range(n_rows)]
     for i in range(n_rows):
         for j in range(n_cols):
-            r = random.randint(0, 3)
+            r = random.randint(0, 2)
             if j == 0 or j == n_cols - 1:
                 mz[i][j] = ['X', 'W']
             else:
-                if r == 0 or r == 1:
+                if r == 0:
                     mz[i][j] = ['X', 'W']
                 else:
                     mz[i][j] = ['_', 'W']
@@ -51,9 +60,9 @@ def print_maze(mz):
     print(' ' * 15, 'exit side')
     print()
 
-maze = generate_maze(8, 10)
+maze = generate_maze(15, 17)
 #entrance as column index
-entrance = 6
+entrance = 10
 maze[0][entrance][0] = '_'
 for i in range(len(maze[0])):
     if i != entrance:
@@ -61,6 +70,7 @@ for i in range(len(maze[0])):
 
 print_maze(maze)
 res = solve(maze, 0, entrance, len(maze), len(maze[0]) - 1)
+print_maze(maze)
 if res:
     print('PATH FOUND')
 else:
