da702c505057950fcf500076b8ae75ebc16ef1ba
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-09-05 23:14:25 +0200

    fix

--- algorithm_drills/huffman.py
+++ algorithm_drills/huffman.py
@@ -1,36 +1,30 @@
-table = {'a': 45, 'b': 13,
-         'c': 12, 'd': 16,
-         'e': 9, 'f': 5}
-
-table_copy = {'a': 45, 'b': 13,
-              'c': 12, 'd': 16,
-              'e': 9, 'f': 5}
-
-tree = {'a': [None, None], 'b': [None, None],
-        'c': [None, None], 'd': [None, None],
-        'e': [None, None], 'f': [None, None]}
-
 def extract_min(d):
   min_v = min(d, key = d.get)
   item = (min_v, d[min_v])
   d.pop(min_v)
   return item
 
-n = len(table)
-i = 0
-
-for j in range(1, n):
+def huffman(table):
+  tree = dict()
+  for j in range(1, len(table)):
     x = extract_min(table)
     y = extract_min(table)
     sum_freqs = x[1] + y[1]
-    node_label = 'n' + str(i)
+    node_label = 'n_' + str(sum_freqs)
     table[node_label] = sum_freqs
-    table_copy[node_label] = sum_freqs
     tree[node_label] = [x[0], y[0]]
-    i += 1
+
+  return tree
+
+table = {'a': 45, 'b': 13,
+         'c': 12, 'd': 16,
+         'e': 9, 'f': 5}
+
+table_copy = table.copy()
 
 print('frequencies:')
 print(table_copy)
+tree = huffman(table)
 print('tree:')
 for it in tree.items():
     print(it)
