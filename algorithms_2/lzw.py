5d64841323fe0f1d993a708708c7890ccf9824a8
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-09-08 23:32:38 +0200

    fix

--- algorithms_2/lzw.py
+++ algorithms_2/lzw.py
@@ -1,13 +1,25 @@
 def lempel_ziv_welch_enc(text, table):
     curr = text[0]
+    codes = []
     for i in range(1, len(text)):
-        curr += text[i]
-        if curr not in table:
-            table.append(curr[0:len(curr)])
-            curr = curr[len(curr) - 1]
+        sym = text[i]
+        if curr + sym in table:
+            curr += sym
+        else:
+            table.append(curr + sym)
+            ind = table.index(curr)
+            codes.append(ind)
+            curr = sym
+
+    ind = table.index(curr)
+    codes.append(ind)
+    return codes
 
 table = [chr(code) for code in range(97, 123)]
-text = 'tobeornottobeortobeornot'
-lempel_ziv_welch_enc(text, table)
+table_cp = table.copy()
+text = 'abcabcabcabcabcabcabcabc'
+enc = lempel_ziv_welch_enc(text, table)
+print(enc)
+print()
 for item in table:
     print(item)
