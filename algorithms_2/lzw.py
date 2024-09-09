45046c77e4687308351585c494a2565513d9544c
Cyofanni <cyofanni@LAPTOP-I0S1RKRC>
2024-09-09 11:32:35 +0200

    add lempel-ziv-welch decoder

--- algorithms_2/lzw.py
+++ algorithms_2/lzw.py
@@ -15,11 +15,35 @@ def lempel_ziv_welch_enc(text, table):
     codes.append(ind)
     return codes
 
+def lempel_ziv_welch_dec(codes, table):
+    read = codes[0]
+    decoded = table[read]
+    S = decoded
+    decoded_string = decoded
+
+    for i in range(1, len(codes)):
+        read = codes[i]
+        prev_decoded = decoded
+        if read < len(table):
+            decoded = table[read]
+            S = prev_decoded + decoded[0]
+            table.append(S)
+        else:
+            decoded = prev_decoded + prev_decoded[0]
+            table.append(decoded)
+        decoded_string += decoded
+
+    return decoded_string
+
 table = [chr(code) for code in range(97, 123)]
 table_cp = table.copy()
-text = 'abcabcabcabcabcabcabcabc'
-enc = lempel_ziv_welch_enc(text, table)
-print(enc)
+text = 'appleqowieuqwappleoiappleapp'
+print('size of uncompressed data:', len(text))
 print()
-for item in table:
-    print(item)
+enc = lempel_ziv_welch_enc(text, table)
+dec = lempel_ziv_welch_dec(enc, table_cp)
+print(enc)
+print('\nsize of compressed data:', len(enc))
+print('\ncheck decompressed == initial data')
+print(dec)
+print(dec == text)
