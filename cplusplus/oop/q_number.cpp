fbadc3de9c46940d852e353c063664b560d8a998
Cyofanni <cyofanni@DESKTOP-L6QM7OU>
2024-10-09 21:13:51 +0200

    add diagonal argument for Q

--- cplusplus/oop/q_number.cpp
+++ cplusplus/oop/q_number.cpp
@@ -6,10 +6,14 @@ class q_number {
     int num, den;
     int gcd() const;
   public:
+    q_number();
     q_number(int, int);
     void reduce();
     void print() const;
 };
+
+q_number::q_number() {
+}
 
 q_number::q_number(int num, int den): num(num), den(den) {
 }
@@ -34,12 +38,72 @@ void q_number::reduce() {
 }
 
 void q_number::print() const {
-  cout << num << '/' << den << endl;
+  cout << num << '/' << den;
 }
 
 int main(int argc, char* argv[]) {
-  q_number q1(20, 15);
-  q1.print();
-  q1.reduce();
-  q1.print();
+  const int matr_size = 8;
+  q_number matr[matr_size][matr_size];
+
+  for (int i = 0; i < matr_size; i++) {
+    for (int j = 0; j < matr_size; j++) {
+      matr[i][j] = q_number(i + 1, j + 1);
+    }
+  }
+  for (int i = 0; i < matr_size; i++) {
+    for (int j = 0; j < matr_size; j++) {
+      matr[i][j].print();
+      cout << ' ';
+    }
+    cout << endl;
+  }
+  cout << endl;
+
+  int row = 0, col = 0;
+  int n = 1;
+
+  while (true) {
+    matr[row][col].print();
+    cout << "    ";
+
+    //down
+    row++;
+    matr[row][col].print();
+    cout << "    ";
+
+    //diag up
+    row--;
+    col++;
+    while (row >= 0) {
+      matr[row][col].print();
+      cout << "    ";
+      row--;
+      col++;
+    }
+    row++;
+    col--;
+
+    if (col == matr_size - 1) {
+      break;
+    }
+
+    //right
+    col++;
+    matr[row][col].print();
+    cout << "    ";
+
+    //diag down
+    row++;
+    col--;
+    while (col > 0) {
+      matr[row][col].print();
+      cout << "    ";
+      row++;
+      col--;
+    }
+
+    cout << endl;
+  }
+
+  cout << endl;
 }
