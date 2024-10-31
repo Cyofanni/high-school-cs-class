1b9a6a9704326209fb7105290c24f7cfe73d8f0a
Cyofanni <cyofanni@DESKTOP-L6QM7OU>
2024-10-31 18:15:13 +0100

    add swap-based insertion sort

---
+++ algorithms_2/insertion_sort_swaps.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  const int size = 16;
+  int ar[size] = {1, 3, -12, 7, -10, 1500, 160, 172, -20, 30, 35, 360, 37, 450, 500, 600};
+
+  for (int i = 1; i < size; i++) {
+    for (int j = i; ar[j] < ar[j - 1]; j--) {
+      int t = ar[j];
+      ar[j] = ar[j - 1];
+      ar[j - 1] = t;
+    }
+  }
+
+  for (int i = 0; i < size; i++) {
+    cout << ar[i] << ' ';
+  }
+  cout << endl;
+}
