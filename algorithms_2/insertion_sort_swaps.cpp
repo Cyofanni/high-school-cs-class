c4b914757611201637d15bf6f09d0e1e1dd7e417
Cyofanni <cyofanni@DESKTOP-L6QM7OU>
2024-11-07 08:40:39 +0100

    fix

--- algorithms_2/insertion_sort_swaps.cpp
+++ algorithms_2/insertion_sort_swaps.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main() {
   const int size = 16;
-  int ar[size] = {1, 3, -12, 7, -10, 1500, 160, 172, -20, 30, 35, 360, 37, 450, 500, 600};
+  int ar[size] = {1, 3, -12, 7, -10, 1500, 160, 172, -20, 30, 35, 360, 37, 450, 500, -80};
 
   for (int i = 1; i < size; i++) {
-    for (int j = i; ar[j] < ar[j - 1]; j--) {
+    for (int j = i; j >= 1 && ar[j] < ar[j - 1]; j--) {
       int t = ar[j];
       ar[j] = ar[j - 1];
       ar[j - 1] = t;
