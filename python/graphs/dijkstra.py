724278c96806ee003ba7f6f4c4ec503b70509502
Cyofanni <cyofanni@DESKTOP-L6QM7OU>
2024-11-21 15:10:52 +0100

    fix

--- python/graphs/dijkstra.py
+++ python/graphs/dijkstra.py
@@ -11,6 +11,7 @@ def dijkstra(g, g_c, g_e, p):
     u = extract_min(Q)
     u_successors = g[u]
     for v in u_successors:
+      #if v in Q:
       edge_name = u + v
       edge_cost = g_e[edge_name]
       if g_c[v] > g_c[u] + edge_cost:
@@ -33,7 +34,7 @@ def print_path_iter(p, v):
   path.insert(0, v)
   print(path)
 
-G = {
+G_0 = {
       's': ['r','t','x'],
       'r': ['t'],
       't': ['y','x'],
@@ -41,7 +42,7 @@ G = {
       'y': []
     }
 
-G_costs = {
+G_costs_0 = {
             's': 0,
             'r': math.inf,
             't': math.inf,
@@ -49,18 +50,17 @@ G_costs = {
             'y': math.inf
           }
 
-G_edges = {
+G_edges_0 = {
             'sr': 5,
             'st': 6,
             'sx': 3,
             'rt': 2,
             'tx': 1,
             'ty': 3,
-            'tx': 1,
             'xy': 9
           }
 
-P = {
+P_0 = {
       's': None,
       'r': None,
       't': None,
@@ -68,48 +68,14 @@ P = {
       'y': None
     }
 
-G_1 = {
-      's': ['r','t','x'],
-      'r': ['t'],
-      't': ['y','x','s'],
-      'x': ['y'],
-      'y': []
-    }
+dijkstra(G_0, G_costs_0, G_edges_0, P_0)
+print(G_costs_0)
+print(P_0)
 
-G_costs_1 = {
-            's': 0,
-            'r': math.inf,
-            't': math.inf,
-            'x': math.inf,
-            'y': math.inf
-          }
-
-G_edges_1 = {
-            'sr': 5,
-            'st': 6,
-            'sx': 3,
-            'rt': 2,
-            'tx': 1,
-            'ty': 3,
-            'tx': 1,
-            'xy': 9,
-            'ts': -10
-          }
-
-P_1 = {
-      's': None,
-      'r': None,
-      't': None,
-      'x': None,
-      'y': None
-    }
-
-dijkstra(G, G_costs, G_edges, P)
-print(G_costs)
-print(P)
-print_path_rec(P, 'y')
-print()
-print_path_rec(P, 't')
-print()
+G_1 = {'A':['B','C'], 'B':['C'], 'C':[]}
+G_costs_1 = {'A': 0, 'B': math.inf, 'C': math.inf}
+G_edges_1 = {'AB': 5, 'AC': 2, 'BC': -10}
+P_1 = {'A': None, 'B': None, 'C': None}
 dijkstra(G_1, G_costs_1, G_edges_1, P_1)
 print(G_costs_1)
+print(P_1)
