06d9fb8e07cd40e9a342afa13d4f136081081aae
Cyofanni <cyofanni@DESKTOP-L6QM7OU>
2025-01-19 18:22:37 +0100

    improve code

--- numerical_analysis/LU_decomposition.py
+++ numerical_analysis/LU_decomposition.py
@@ -1,9 +1,9 @@
 import numpy as np
 
 def LU_decomposition(A):
-    n = len(A)
-    L = [[0 for _ in range(n)] for _ in range(n)]
-    U = [[0 for _ in range(n)] for _ in range(n)]
+    n = A.shape[0]
+    L = np.array([[0 for _ in range(n)] for _ in range(n)])
+    U = np.array([[0 for _ in range(n)] for _ in range(n)])
     U[0] = A[0].copy()
 
     for i in range(n):
@@ -17,20 +17,14 @@ def LU_decomposition(A):
 
     return (L, U)
 
-A = [[2,3,1,5],[6,13,5,19],[2,19,10,23],[4,10,11,31]]
-for row in A:
-    print(row)
+A = np.array([[2,3,1,5],[6,13,5,19],[2,19,10,23],[4,10,11,31]])
+print(A)
 print()
-L = LU_decomposition(A)[0]
-U = LU_decomposition(A)[1]
+L, U = LU_decomposition(A)[0], LU_decomposition(A)[1]
 
-for row in L:
-    print(row)
+print(L)
 print()
-for row in U:
-    print(row)
+print(U)
 
 print()
-prod = np.matmul(L, U)
-for row in prod:
-    print(row)
+print(np.matmul(L, U))
