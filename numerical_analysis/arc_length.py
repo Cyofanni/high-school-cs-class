3b94a0d0ea664a80187e2508c9750fc7e3965a46
Cyofanni <cyofanni@DESKTOP-L6QM7OU>
2025-01-29 14:17:35 +0100

    fix

--- numerical_analysis/arc_length.py
+++ numerical_analysis/arc_length.py
@@ -5,9 +5,9 @@ def f0(x):
 
 def arc_length_numerical(f, a, b, n):
   dx = (b - a) / n
-  xs = [i * dx for i in range(n + 1)]
+  xs = [a + i * dx for i in range(0, n + 1)]
   ys = [f(x) for x in xs]
   ls = [np.sqrt((xs[i]-xs[i-1])**2 + (ys[i]-ys[i-1])**2) for i in range(1, n + 1)]
   return sum(ls)
 
-print(arc_length_numerical(f0, 0, 6, 100))
+print(arc_length_numerical(f0, -6, 6, 100))
