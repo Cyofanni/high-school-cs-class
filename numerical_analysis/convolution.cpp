2f67b5c496532467a9ebfc31f3c95e67199d2e05
Cyofanni <cyofanni@DESKTOP-L6QM7OU.localdomain>
2025-09-29 09:34:02 +0200

    fix

--- numerical_analysis/convolution.cpp
+++ numerical_analysis/convolution.cpp
@@ -2,10 +2,10 @@
 using namespace std;
 
 int main() {
-  double P[] = {-8, 6 , 3, -4, 0, 0};    //-4x^3 + 3x^2 + 6x - 8
-  double Q[] = {16, 10, 4, 0, 0, 0};     //4x^2 + 10x + 16
+  double P[] = {3, 2, 6, 1, 12, 0, 0, 0, 0};    //3 + 2x + 6x^2 + 1x^3 + 12x^4
+  double Q[] = {7, 6, 1, -6, 3, 0, 0, 0, 0};    //7 + 6x + 1x^2 - 6x^3 + 3x^4
 
-  const int conv_size = 6;
+  const int conv_size = 9;
   double convolution[conv_size] = {};
 
   for (int i = 0; i < conv_size; i++) {
