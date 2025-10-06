c0fb3f364b55195895da4a63d499d9b38b943a06
Cyofanni <cyofanni@DESKTOP-L6QM7OU.localdomain>
2025-10-06 10:30:10 +0200

    fix

--- cplusplus/oop/game_0.cpp
+++ cplusplus/oop/game_0.cpp
@@ -53,10 +53,10 @@ public:
     }
     for (int i = 0; i < size; i++) {
       for (int j = 0; j < ulc_x; j++) {
-	cout << ' ';
+        cout << ' ';
       }
       for (int j = 0; j < size; j++) {
-	cout << '*';
+        cout << '*';
       }
       cout << endl;
     }
