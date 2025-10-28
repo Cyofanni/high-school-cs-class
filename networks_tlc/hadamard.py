b7640ec03b55c4317330ac8dbcab107fad90c7a6
Cyofanni <cyofanni@DESKTOP-L6QM7OU.localdomain>
2025-10-28 15:27:56 +0100

    fix

--- networks_tlc/hadamard.py
+++ networks_tlc/hadamard.py
@@ -4,7 +4,7 @@ def hadamard(n):
   if n == 0:
     return np.array([1])
   h_ul = hadamard(n - 1)
-  h_lr = -1 * hadamard(n - 1)
+  h_lr = -1 * h_ul
   h_u = np.hstack((h_ul, h_ul))
   h_l = np.hstack((h_ul, h_lr))
   return np.vstack((h_u, h_l))
