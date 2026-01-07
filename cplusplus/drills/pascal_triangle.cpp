88b469c901af4669859b66885950d0198d9351b2
Giovanni Mazzocchin <giovanni.mazzocchin@gmail.com>
2026-01-07 15:08:39 +0100

    Update pascal_triangle.cpp

--- cplusplus/drills/pascal_triangle.cpp
+++ cplusplus/drills/pascal_triangle.cpp
@@ -1,13 +1,14 @@
 #include <iostream>
 using namespace std;
 
-#define NUM_ROWS_PASCAL 8
-#define NUM_COLS_PASCAL 15
+#define NUM_ROWS_PASCAL 6
+#define NUM_COLS_PASCAL NUM_ROWS_PASCAL * 2 - 1
 
 int main() {
-  int pascal_triangle[NUM_ROWS_PASCAL][NUM_COLS_PASCAL] = {{0,0,0,0,0,0,0,1,0,0,0,0,0,0,0},
-                                                           {}, {}, {}, {}, {}, {}, {}};
-  int first_index_col = 6, last_index_col = 8;
+  int pascal_triangle[NUM_ROWS_PASCAL][NUM_COLS_PASCAL] = {};
+  pascal_triangle[0][(NUM_COLS_PASCAL - 1) / 2] = 1;
+  int first_index_col = (NUM_COLS_PASCAL - 1) / 2 - 1;
+  int last_index_col = (NUM_COLS_PASCAL - 1) / 2 + 1;
   /*
       0 0 0 0 1 0 0 0 0
       0 0 0 1 0 1 0 0 0
@@ -18,11 +19,11 @@ int main() {
   */
   for (int i = 1; i < NUM_ROWS_PASCAL; i++) {
     for (int j = first_index_col; j <= last_index_col; j += 2) {
-      if (j >= 1 && j <= 13) {
-        pascal_triangle[i][j] = pascal_triangle[i - 1][j - 1] + pascal_triangle[i - 1][j + 1];
+      if (j == 0 || j == NUM_COLS_PASCAL - 1) {
+        pascal_triangle[i][j] = 1;
       }
       else {
-        pascal_triangle[i][j] = 1;
+        pascal_triangle[i][j] = pascal_triangle[i - 1][j - 1] + pascal_triangle[i - 1][j + 1];
       }
     }
     first_index_col--;
@@ -33,9 +34,6 @@ int main() {
     for (int j = 0; j < NUM_COLS_PASCAL; j++) {
       if (pascal_triangle[i][j] != 0) {
         cout << pascal_triangle[i][j] << ' ';
-      }
-      else {
-        cout << ' ';
       }
     }
     cout << endl;
