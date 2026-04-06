639f500250db8ada5eaa60ae7b3653aee5754551
Cyofanni <cyofanni@cyofanni.localdomain>
2026-04-06 14:39:53 +0200

    fix

--- javascript/bin_nums.js
+++ javascript/bin_nums.js
@@ -2,22 +2,22 @@ nums = [];
 
 function bin_nums(n) {
     if (n == 0) {
-	return;
+        return;
     }
     if (n == 1) {
-	nums.push([0], [1]);
-	return;
+        nums.push([0], [1]);
+        return;
     }
     bin_nums(n - 1);
     let half_len = nums.length;
     for (let i = 0; i < half_len; i++) {
-	nums.push(nums[i].slice(0, nums[i].length));
+        nums.push(nums[i].slice(0, nums[i].length));
     }
     for (let i = 0; i < half_len; i++) {
-	nums[i].splice(0, 0, 0);
+        nums[i].splice(0, 0, 0);
     }
     for (let i = half_len; i < half_len * 2; i++) {
-	nums[i].splice(0, 0, 1);
+        nums[i].splice(0, 0, 1);
     }
 }
 
