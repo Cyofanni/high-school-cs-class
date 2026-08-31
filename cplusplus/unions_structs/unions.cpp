66a56c286d2540ed060775a3ef062d86854c936c
Cyofanni <cyofanni@cyofanni.localdomain>
2026-08-31 15:24:31 +0200

    fix

--- cplusplus/unions_structs/unions.cpp
+++ cplusplus/unions_structs/unions.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 union var_rec {
-	unsigned short usi;
-	unsigned int ui;
-	unsigned long int uli;
-        float d;
+  unsigned short usi;
+  unsigned int ui;
+  unsigned long int uli;
+  float d;
 };
 
 int main() {
-	union var_rec v;
-	v.uli = 0xCAFEDEADBEBEBABA;
-	cout << v.usi << endl;
-	cout << v.ui << endl;
-	cout << v.uli << endl;
-        cout << setprecision(12) << v.d << endl;
+  union var_rec v;
+  v.uli = 0xCAFEDEADBEBEBABA;
+  cout << v.usi << endl;
+  cout << v.ui << endl;
+  cout << v.uli << endl;
+  cout << setprecision(12) << v.d << endl;
 }
